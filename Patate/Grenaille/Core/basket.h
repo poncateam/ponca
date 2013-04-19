7bb8ca44cd409098d60f31c40f93de8875ce9921
Nicolas Mellado <nicolas.mellado@inria.fr>
2013-04-19 11:44:53 +0200

    Add extensions

--- Patate/Grenaille/Core/basket.h
+++ Patate/Grenaille/Core/basket.h
@@ -19,12 +19,10 @@ namespace Grenaille{
 
    */
   template < class P, class W, template <class, class, typename>class Fit, 
-	     BASKET_TP(0),  BASKET_TP(1), BASKET_TP(2), BASKET_TP(3), BASKET_TP(4), BASKET_TP(5) >
+	     BASKET_TP(0), BASKET_TP(1), BASKET_TP(2), BASKET_TP(3), BASKET_TP(4), BASKET_TP(5), BASKET_TP(6), BASKET_TP(7), BASKET_TP(8), BASKET_TP(9), BASKET_TP(10), BASKET_TP(11) >
   class Basket
-    : public Ext5<P,W,Ext4<P,W, Ext3<P,W, Ext2<P,W, Ext1<P,W, Ext0<P,W, Fit<P,W,void> > > > > > >
+    : public Ext11<P,W, Ext10<P,W, Ext9<P,W, Ext8<P,W, Ext7<P,W, Ext6<P,W, Ext5<P,W, Ext4<P,W, Ext3<P,W, Ext2<P,W, Ext1<P,W, Ext0<P,W, Fit<P,W,void> > > > > > > > > > > > > 
   {
-    //  private:
-    //    typedef Ext5<S,D,W,Ext4<S,D,W, Ext3<S,D,W, Ext2<S,D,W, Ext1<S,D,W, Ext0<S,D,W, Fit<S,D,W> > > > > > > Base;
   }; // class Basket
 
 #undef BASKET_TP
