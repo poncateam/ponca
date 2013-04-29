53b8cfdc5be1a760d4e1ea326d64f62e2ff86348
Nicolas Mellado <nicolas.mellado@inria.fr>
2013-04-29 13:32:38 +0200

    Update documentation

--- Patate/grenaille.h
+++ Patate/grenaille.h
@@ -18,4 +18,60 @@
 
 // Include Grenaille Algorithms
 
+
+
+/*!
+ *  \addtogroup Grenaille
+ *  @{
+ */
+
+
+/*!
+
+  \brief Documentation
+
+  Toto
+
+  You must define a Point class:
+  \code
+  // Define our working data structure
+class MyPoint{
+public:
+  enum {Dim = 3};
+  typedef double Scalar;
+  typedef Eigen::Matrix<Scalar, Dim, 1> VectorType;
+
+  MULTIARCH inline MyPoint(const VectorType &pos    = VectorType::Zero(), 
+		 const VectorType& normal = VectorType::Zero())
+    : _pos(pos), _normal(normal) {}
+    
+  MULTIARCH inline const VectorType& pos()    const { return _pos; }  
+  MULTIARCH inline const VectorType& normal() const { return _normal; }
+
+  MULTIARCH inline VectorType& pos()    { return _pos; }  
+  MULTIARCH inline VectorType& normal() { return _normal; }
+
+  static inline MyPoint Random() { 
+    return MyPoint (VectorType::Random(), VectorType::Random());
+  };
+
+private:
+  VectorType _pos, _normal;
+};
+  \endcode
+
+  and define
+  \code
+typedef DistWeightFunc<MyPoint,SmoothWeightKernel<Scalar> > WeightFunc; 
+typedef Basket<MyPoint,WeightFunc,OrientedSphereFit, GLSParam, OrientedSphereScaleDer, GLSDer, GLSGeomVar> Fit;
+  \endcode
+ */
+namespace Grenaille
+{
+
+
+} // End namespace Grenaille
+
+/*! @} End of Doxygen Groups*/
+
 #endif //_PATATE_GRENAILLE_
