a69e261a34d116657f2bd21d926cc3028a33712c
nmellado <nmellado@NicoLinux>
2013-05-09 18:54:58 +0200

    Grenaille documentation

--- Patate/grenaille.h
+++ Patate/grenaille.h
@@ -35,7 +35,7 @@
   \section grenaille_overview_sec Overview
 
   In Grenaille, you have access to Fitting Kernels and Extensions you can 
-  combine using the #Basket template class to generate a specialized 
+  combine using the \ref Basket template class to generate a specialized 
   fitting or analysis kernel:
   \image html grenaille/concept.svg
 
@@ -54,51 +54,89 @@
 
   In practice, you will have to define a Point class (you could use the example below
   as basis), a weighting function (most of the time you could use those provided in 
-  Grenaille), choose your fitting kernel and the associated extension. The interface
-  of your class Point is defined the requirement of the weighting, fitting and 
+  Grenaille), choose your fitting kernel and the associated extensions. The interface
+  of your class Point is defined by the requirements of the weighting, fitting and 
   extension classes.
+
+  An example of Basket, to fit spheres on oriented point-sets and compute the GLS 
+  descriptor:
+  \code
+  typedef Basket<MyPoint,WeightFunc,OrientedSphereFit, GLSParam> Fit;
+  \endcode
 
   \section grenaille_howto_sec How to
 
   \subsection grenaille_howto_point_sec Point
 
-  \subsection grenaille_howto_fit_sec Fitting kernel
-
-  \subsection grenaille_howto_ext_sec Extensions
-
-  You must define a Point class:
+  You must implemente the following interface to write your Point class:
+    - Define a public enum value called Dim, to define the number of dimensions:
   \code
-  // Define our working data structure
+enum {Dim = 3};
+  \endcode
+    - Define a public Scalar type:
+  \code
+typedef float Scalar; //use floating precision
+  \endcode
+    - Define a public Vector type. It is strongly recommended to use an Eigen type:
+  \code
+typedef Eigen::Matrix<Scalar, Dim, 1> VectorType;
+  \endcode
+    - Provide an implicit constructor,
+    - Provide at least a position vector, with r/w accessors:
+  \code
+  MULTIARCH const VectorType& pos()const;
+  MULTIARCH       VectorType& pos();
+  \endcode
+  
+  Example of a minimal class:
+  \code
 class MyPoint{
 public:
   enum {Dim = 3};
-  typedef double Scalar;
+  typedef float Scalar;
   typedef Eigen::Matrix<Scalar, Dim, 1> VectorType;
 
-  MULTIARCH inline MyPoint(const VectorType &pos    = VectorType::Zero(), 
-		 const VectorType& normal = VectorType::Zero())
-    : _pos(pos), _normal(normal) {}
+  MULTIARCH inline MyPoint(const VectorType &pos    = VectorType::Zero())
+    : _pos(pos) {}
     
   MULTIARCH inline const VectorType& pos()    const { return _pos; }  
-  MULTIARCH inline const VectorType& normal() const { return _normal; }
-
   MULTIARCH inline VectorType& pos()    { return _pos; }  
-  MULTIARCH inline VectorType& normal() { return _normal; }
-
-  static inline MyPoint Random() { 
-    return MyPoint (VectorType::Random(), VectorType::Random());
-  };
 
 private:
-  VectorType _pos, _normal;
+  VectorType _pos;
 };
   \endcode
 
-  and define
+  The other requirements defined by the \ref Basket specialization are listed 
+  in the documentation of each used class (Weigthing function, fit or 
+  extension). The following sections present at least these requirements for 
+  the common cases.
+
+  \subsection grenaille_howto_fit_sec Fitting kernel
+  \warning Documentation not written yet
+
+  \subsection grenaille_howto_weight_sec Weighting function
+
+  There is two components combined to weight samples:
+    - a 1D kernel, defined in the interval \f$ \left[0 \; : \; 1 \right]\f$. 
+  Actually their is two kernels included in the library: 
+  \ref ConstantWeightKernel and \ref SmoothWeightKernel. Please go to the 
+  related documentation pages for more details.
+    - a specialized function that will use this kernel with respect to a given
+  point structure. It must derive from \ref BaseWeightFunc. Actually only 
+  \ref DistWeightFunc is provided, to compute a weight with respect to the
+  distance between samples for a given scale \f$ t \f$ specified at runtime.
+
+  An example of Weighting function with the previous MyPoint class:
   \code
 typedef DistWeightFunc<MyPoint,SmoothWeightKernel<Scalar> > WeightFunc; 
-typedef Basket<MyPoint,WeightFunc,OrientedSphereFit, GLSParam, OrientedSphereScaleDer, GLSDer, GLSGeomVar> Fit;
   \endcode
+
+  \subsection grenaille_howto_ext_sec Extensions
+  \warning Documentation not written yet
+
+
+
  */
 namespace Grenaille
 { 
