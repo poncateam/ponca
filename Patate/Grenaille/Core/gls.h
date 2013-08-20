70183e8748a891b27c6bf48e0b6e40c19793492d
Nicolas Mellado <nicolas.mellado@inria.fr>
2013-08-20 16:41:41 +0200

    Add helper class to compute principal curvatures

--- Patate/Grenaille/Core/gls.h
+++ Patate/Grenaille/Core/gls.h
@@ -4,6 +4,7 @@
  file, You can obtain one at http://mozilla.org/MPL/2.0/. 
 */
 
+#include <Eigen/Eigenvalues>
 
 #ifndef _GRENAILLE_GLS_
 #define _GRENAILLE_GLS_
@@ -190,6 +191,117 @@ namespace Grenaille
     MULTIARCH inline Scalar geomVar(Scalar wtau   = Scalar(1), 
   			                            Scalar weta   = Scalar(1),
 	                        			    Scalar wkappa = Scalar(1)) const;
+  };
+  
+  
+  /*!
+    \brief Extension to compute curvature values from \f$ \frac{\delta\tau}{\tau\mathbf{x}} \f$
+    \inherit Concept::FittingExtensionConcept
+    
+    This class requires an Eigendecomposition of the Jacobian matrix of 
+    \f$ \tau \f$. We use an Eigen::SelfAdjointEigenSolver, compatible with 
+    Eigen-nvcc for 2D and 3D spaces on cuda, and in arbitrary dimension for CPU
+    compilation (cuda version also use a fast but less accurate decomposition
+    provided by computeDirect. See <a href="http://eigen.tuxfamily.org/dox/classEigen_1_1SelfAdjointEigenSolver.html#a85cda7e77edf4923f3fc0512c83f6323" target="_blank">Eigen documentation</a> for mor details).
+   */
+  template < class DataPoint, class _WFunctor, typename T>
+  class GLSCurvatureHelper : public T{
+  private:
+    typedef T Base;
+
+  protected:
+    enum
+      {
+        Check = Base::PROVIDES_ALGEBRAIC_SPHERE_SPACE_DERIVATIVE & Base::PROVIDES_GLS_DERIVATIVE,
+	      PROVIDES_PRINCIPALE_CURVATURES
+      };
+
+  public:
+    typedef typename Base::Scalar     Scalar;
+    typedef typename Base::VectorType VectorType;
+    typedef typename Base::MatrixType MatrixType;
+      
+  private:
+    Scalar _k1, _k2;
+    VectorType _v1, _v2;
+
+    
+  public:
+    /*! \brief Default constructor */
+    MULTIARCH inline GLSCurvatureHelper() : _k1(0), _k2(0) {}
+    
+    /**************************************************************************/
+    /* Processing                                                             */
+    /**************************************************************************/
+    /*! \copydoc Concept::FittingProcedureConcept::finalize() */
+    MULTIARCH inline void finalize   (){
+	    MULTIARCH_STD_MATH(sqrt);
+	      
+      Base::finalize();
+      
+      // Extract the spatial variations of eta
+      MatrixType jacobian = Base::deta().template middleCols<DataPoint::Dim>(Base::isScaleDer() ? 1: 0);
+      
+      // Use a simple solver with 2x2 and 3x3 closed forms compatible with eigen-nvcc
+      // This solver requires a triangular matrix
+      Eigen::SelfAdjointEigenSolver<MatrixType> eig;
+#ifdef __CUDACC__
+      eig.computeDirect(jacobian.transpose()*jacobian);
+#else
+      eig.compute(jacobian.transpose()*jacobian);
+#endif
+      
+      // Need sqrt because we solve eigendecomposition of JT * J.
+      _k1 = sqrt(eig.eigenvalues()(2)); 
+      _k2 = sqrt(eig.eigenvalues()(1)); 
+      
+      _v1 = eig.eigenvectors().col(2);
+      _v2 = eig.eigenvectors().col(1);
+      
+      // Now check the sign of the mean curvature to detect if we need to change
+      // the sign of the principal curvature values:
+      // the eigen decomposition return k1 and k2 wrt k1*k1 > k2*k2
+      
+      // Compare with the values of the mean curvature computed without k1 and k2
+      Scalar H2 = Scalar(2)*Base::kappa(); // we could also use the trace of the
+                                           // jacobian matrix to get mean curvature
+      
+      // Change sign and flip values if needed
+      if( H2 == Scalar(0)){
+        _k2 = -_k2;        
+        
+      } else if ( H2 > Scalar(0) ) {
+        if( H2 < _k1 )
+          _k2 = -_k2;
+      }else { // 2H < 0. In this case, we have k1<0, and k1 < k2
+        if( H2 > _k1 )
+          _k2 = -_k2;
+        _k1 = -_k1;
+        
+        // need to invert k1 and k2, and get the corresponding vectors
+        Scalar tmp = _k1; _k1 = _k2; _k2 = tmp;        
+        _v1 = eig.eigenvectors().col(1);
+        _v2 = eig.eigenvectors().col(2);        
+      }
+    }
+    
+    /**************************************************************************/
+    /* Use results                                                            */
+    /**************************************************************************/
+    //! \brief Return the first principal curvature value
+    MULTIARCH inline Scalar GLSk1() const { return _k1; }
+    
+    //! \brief Return the second principal curvature value
+    MULTIARCH inline Scalar GLSk2() const { return _k2; }
+    
+    //! \brief Return the first principal curvature direction
+    MULTIARCH inline VectorType GLSk1Direction() const { return _v1; }
+    
+    //! \brief Return the second principal curvature direction
+    MULTIARCH inline VectorType GLSk2Direction() const { return _v2; }
+    
+    //! \brief Return the Gaussian Curvature
+    MULTIARCH inline Scalar GLSGaussianCurvature() const { return _k1*_k2;}    
   };
   
   #include "gls.hpp"
