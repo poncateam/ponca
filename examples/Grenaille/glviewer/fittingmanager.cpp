e7a66620fd6b7f9189fea710cf61f3c1bc854639
Nicolas Mellado <nmellado0@gmail.com>
2016-02-24 10:45:45 +0100

    First version of the manager, allowing to set basic parameters, and fit the primitive

--- examples/Grenaille/glviewer/fittingmanager.cpp
+++ examples/Grenaille/glviewer/fittingmanager.cpp
@@ -2,11 +2,75 @@
 
 FittingManager::FittingManager(QObject *parent) :
     QObject(parent),
-    _fitType(FittingManager::UNSUPPORTED),
-    _mesh(NULL)
+    _fitType(FittingManager::PLANE_COV),
+    _mesh(NULL),
+    _scale(0.02),
+    _evalPos(Mesh::Vector::Zero())
 {
 }
 void
 FittingManager::setBasketType(FIT_TYPE type){
     _fitType = type;
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+
+    fitPrimitive();
+}
+
+void
+FittingManager::setEvaluationPoint(const PatateCommon::GLTri3DMesh::Vector &pos){
+    _evalPos = pos;
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+
+    fitPrimitive();
+}
+
+void
+FittingManager::setScale(float scale){
+    _scale = scale;
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+
+    fitPrimitive();
+}
+
+void
+FittingManager::setScale(double scale){
+    _scale = scale;
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+
+    fitPrimitive();
+}
+
+void
+FittingManager::fitPrimitive(){
+#define COMPUTE(T)                                                             \
+    typedef typename fittingmanagerspace::BasketMaker<T>::Basket Fit;          \
+    typedef typename fittingmanagerspace::BasketMaker<T>::WeightFunc WFunc;    \
+    Fit f;                                                                     \
+    f.setWeightFunc(WFunc(_scale));                                            \
+    f.init(_evalPos);                                                          \
+    f.compute(_mesh->begin(), _mesh->end());
+
+
+    switch(_fitType){
+    case PLANE_COV:
+    {
+        COMPUTE(PLANE_COV);
+        std::cout << "Normal vector:     " << f.primitiveGradient(_evalPos).transpose() << std::endl;
+        std::cout << "Surface variation: " << f.surfaceVariation() << std::endl;
+        break;
+    }
+    case SPHERE_ORIENTED:
+    {
+        COMPUTE(SPHERE_ORIENTED);
+        std::cout << "Tau:   " << f.tau() << std::endl;
+        std::cout << "Eta:   " << f.eta().transpose() << std::endl;
+        std::cout << "Kappa: " << f.kappa() << std::endl;
+        break;
+    }
+    case UNSUPPORTED:
+    default:
+        return;
+    }
+
+    emit fitPerformed();
 }
--- examples/Grenaille/glviewer/fittingmanager.h
+++ examples/Grenaille/glviewer/fittingmanager.h
@@ -20,16 +20,27 @@ public:
         UNSUPPORTED
     };
 
-    inline void setMesh(PatateCommon::GLTri3DMesh* mesh) { _mesh = mesh; }
+    typedef PatateCommon::GLTri3DMesh Mesh;
+    typedef typename Mesh::Scalar Scalar;
+
+    inline void setMesh(Mesh* mesh) { _mesh = mesh; }
 
 signals:
+    //! \brief Signal emitted when the basket has been applied to the input data.
+    void fitPerformed();
 
 public slots:
     void setBasketType(FIT_TYPE type);
+    void setEvaluationPoint(const PatateCommon::GLTri3DMesh::Vector &pos);
+    void setScale(float scale);
+    void setScale(double scale);
+    void fitPrimitive();
 
 private:
     FIT_TYPE _fitType;
     PatateCommon::GLTri3DMesh *_mesh;
+    Scalar _scale;
+    typename Mesh::Vector _evalPos;
 };
 
 namespace fittingmanagerspace {
@@ -49,7 +60,8 @@ template <>
 struct BasketMaker<FittingManager::SPHERE_ORIENTED>{
     typedef Grenaille::DistWeightFunc<MyPoint,Grenaille::SmoothWeightKernel<MyPoint::Scalar> > WeightFunc;
     typedef Grenaille::Basket<MyPoint,WeightFunc, Grenaille::AlgebraicSphere,
-                                                  Grenaille::OrientedSphereFit> Basket;
+                                                  Grenaille::OrientedSphereFit,
+                                                  Grenaille::GLSParam> Basket;
 };
 
 } // namespace fittingmanagerspace
