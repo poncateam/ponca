2abeb8434ffdaedd89c637e92e89512a66c3cc95
thibault <thibault.le@live.fr>
2018-03-05 01:29:21 +0100

    add curvatureEstimation.h file in grenaille.h

--- Patate/grenaille.h
+++ Patate/grenaille.h
@@ -1,4 +1,4 @@
-/*
+﻿/*
  This Source Code Form is subject to the terms of the Mozilla Public
  License, v. 2.0. If a copy of the MPL was not distributed with this
  file, You can obtain one at http://mozilla.org/MPL/2.0/. 
@@ -27,6 +27,8 @@
 #include "Grenaille/Core/orientedSphereFit.h"
 #include "Grenaille/Core/curvature.h"
 #include "Grenaille/Core/gls.h"
+
+#include "Grenaille/Core/curvatureEstimation.h"
 
 // not supported on cuda
 #ifndef __CUDACC__
