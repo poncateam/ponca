54a6136fc4ae5f11061ffec3c3db655197562a90
Nicolas Mellado <nmellado0@gmail.com>
2018-05-25 17:47:56 +0200

    Add Sphere fit without normal

---
+++ Patate/Grenaille/Core/sphereFit.hpp
@@ -0,0 +1,105 @@
+/*
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+
+template < class DataPoint, class _WFunctor, typename T>
+void
+SphereFit<DataPoint, _WFunctor, T>::init(const VectorType& _evalPos)
+{
+    // Setup primitive
+    Base::resetPrimitive();
+    Base::basisCenter() = _evalPos;
+
+    // Setup fitting internal values
+    m_sumW = Scalar(0.0);
+    m_matA.setZero();
+}
+
+template < class DataPoint, class _WFunctor, typename T>
+bool
+SphereFit<DataPoint, _WFunctor, T>::addNeighbor(const DataPoint& _nei)
+{
+    // centered basis
+    VectorType q = _nei.pos() - Base::basisCenter();
+
+    // compute weight
+    Scalar w = m_w.w(q, _nei);
+
+    if (w > Scalar(0.))
+    {
+        VectorA a;
+        a << 1, q, q.squaredNorm();
+        m_matA     += w * a * a.transpose();
+        m_sumW     += w;
+
+        ++(Base::m_nbNeighbors);
+        return true;
+    }
+
+    return false;
+}
+
+
+template < class DataPoint, class _WFunctor, typename T>
+FIT_RESULT
+SphereFit<DataPoint, _WFunctor, T>::finalize ()
+{
+    // handle specific configurations
+    // With less than 3 neighbors the fitting is undefined
+    if(m_sumW == Scalar(0.) || Base::m_nbNeighbors < 3)
+    {
+        Base::m_ul.setZero();
+        Base::m_uc = Scalar(0.);
+        Base::m_uq = Scalar(0.);
+        Base::m_isNormalized = false;
+        Base::m_eCurrentState = UNDEFINED;
+        return Base::m_eCurrentState;
+    }
+
+    MatrixA matC;
+    matC.setIdentity();
+    matC.template topRightCorner<1,1>()    << -2;
+    matC.template bottomLeftCorner<1,1>()  << -2;
+    matC.template topLeftCorner<1,1>()     << 0;
+    matC.template bottomRightCorner<1,1>() << 0;
+
+    MatrixA invCpratt;
+    invCpratt.setIdentity();
+    invCpratt.template topRightCorner<1,1>()    << -0.5;
+    invCpratt.template bottomLeftCorner<1,1>()  << -0.5;
+    invCpratt.template topLeftCorner<1,1>()     << 0;
+    invCpratt.template bottomRightCorner<1,1>() << 0;
+
+    MatrixA M = invCpratt * m_matA;
+    Eigen::EigenSolver<MatrixA> eig(M);
+    VectorA eivals = eig.eigenvalues().real();
+    int minId = -1;
+    for(int i=0 ; i<DataPoint::Dim+2 ; ++i)
+    {
+    Scalar ev = eivals(i);
+    if((ev>0) && (minId==-1 || ev<eivals(minId)))
+    minId = i;
+    }
+
+    //mLambda = eivals(minId);
+    VectorA vecU = eig.eigenvectors().col(minId).real();
+    Base::m_uq = vecU[1+DataPoint::Dim];
+    Base::m_ul = vecU.template segment<DataPoint::Dim>(1);
+    Base::m_uc = vecU[0];
+
+    Base::m_isNormalized = false;
+
+    if(Base::m_nbNeighbors < 6)
+    {
+        Base::m_eCurrentState = UNSTABLE;
+    }
+    else
+    {
+        Base::m_eCurrentState = STABLE;
+    }
+
+    return Base::m_eCurrentState;
+}
--- Patate/grenaille.h
+++ Patate/grenaille.h
@@ -1,7 +1,7 @@
 ﻿/*
  This Source Code Form is subject to the terms of the Mozilla Public
  License, v. 2.0. If a copy of the MPL was not distributed with this
- file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
 
@@ -34,6 +34,7 @@
 // not supported on cuda
 #ifndef __CUDACC__
 # include "Grenaille/Core/unorientedSphereFit.h"
+# include "Grenaille/Core/sphereFit.h"
 #endif
 
 
