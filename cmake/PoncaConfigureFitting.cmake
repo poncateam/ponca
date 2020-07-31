edd0178bd93095b584d58ae4c6951f9d0a40db9a
nmellado <nmellado0@gmail.com>
2020-07-31 09:41:43 +0200

    Fix export for Fitting module

--- cmake/PoncaConfigureFitting.cmake
+++ cmake/PoncaConfigureFitting.cmake
@@ -42,19 +42,26 @@ target_sources(Fitting INTERFACE
     "$<BUILD_INTERFACE:${ponca_Fitting_INCLUDE}>"
     "$<INSTALL_INTERFACE:>"
     )
-
+add_dependencies(Fitting Common)
 
 set_target_properties(Fitting PROPERTIES
   INTERFACE_COMPILE_FEATURES cxx_std_11
 )
 
-#target_link_libraries(Fitting PUBLIC INTERFACE Eigen3::Eigen)
+target_link_libraries(Fitting PUBLIC INTERFACE Eigen3::Eigen)
 
 install(TARGETS Fitting
     EXPORT FittingTargets
     LIBRARY DESTINATION  ${CMAKE_INSTALL_LIBDIR}
     ARCHIVE DESTINATION  ${CMAKE_INSTALL_LIBDIR}
     INCLUDES DESTINATION ${CMAKE_INSTALL_INCDIR}
+)
+
+install(EXPORT FittingTargets
+  FILE PoncaTargets-Fitting.cmake
+  NAMESPACE Ponca::
+  DESTINATION lib/cmake
+  COMPONENT Common
 )
 
 add_library(Ponca::Fitting ALIAS Fitting)
