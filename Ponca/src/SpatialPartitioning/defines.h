b8bd6121bd375453c33f78ee01bb4ba0bc93a239
nmellado <nmellado0@gmail.com>
2020-07-31 12:01:08 +0200

    Add package SpacePartitioning + Query base classes

---
+++ Ponca/src/SpatialPartitioning/defines.h
@@ -0,0 +1,26 @@
+/*
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+
+#pragma once
+
+namespace Ponca {
+/**
+  *
+  * \defgroup spatialpartitioning Spatial Partitioning module
+  * \brief This modules includes classes and methods for Spatial Partitioning and queries. See reference manual below.
+  * @{
+  * @}
+  *
+  */
+
+/// Scalar type used in SpatialPartitioning module
+/// \ingroup spatialpartitioning
+using SPScalar = float;
+
+} // namespace pca
+
+#include "../Common/defines.h"
