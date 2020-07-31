b631e88b9e87d487f283f2c4c3d74cfe450d7458
nmellado <nmellado0@gmail.com>
2020-07-31 12:01:08 +0200

    Add package SpacePartitioning + Query base classes

---
+++ Ponca/src/SpatialPartitioning/indexSquaredDistance.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <limits>
+#include "./defines.h"
+
+namespace Ponca {
+
+/// \brief Associates an index with a distance
+/// \ingroup spatialpartitioning
+struct IndexSquaredDistance
+{
+    //// Index of the closest point
+    int index {-1};
+
+    /// Distance to the closest point
+    SPScalar squared_distance { std::numeric_limits<SPScalar>::max() };
+
+    /// Comparison operator based on squared_distance
+    inline bool operator < (const IndexSquaredDistance& other) const
+    { return squared_distance < other.squared_distance; }
+};
+
+} // namespace pca
