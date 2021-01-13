ec7354aa680914927d8365bec8daeb5ed997e03a
Nicolas Mellado <nmellado0@gmail.com>
2021-01-13 14:29:15 +0100

    Explicitly define M_PI if not defined

--- tests/common/testing.h
+++ tests/common/testing.h
@@ -21,6 +21,11 @@
 #	include <cmath>
 #endif
 
+#ifndef M_PI
+// Source: http://www.geom.uiuc.edu/~huberty/math5337/groupe/digits.html
+#define M_PI 3.141592653589793238462643383279502884197169399375105820974944592307816406
+#endif
+
 #ifdef PONCA_COVERAGE_ENABLED
 #define DEFAULT_REPEAT 1
 #else
