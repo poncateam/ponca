0add94ea2f1d5d03837c54ab071d890c48f9078b
sachavincent <sacha.vincent@univ-tlse3.fr>
2021-02-03 10:27:32 +0100

    Added iterators

---
+++ Ponca/src/SpatialPartitioning/KdTree/Iterator/KdTreeKNearestPointIterator.hpp
@@ -0,0 +1,36 @@
+/*
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#include "./kdTreeKNearestPointIterator.h"
+
+namespace Ponca {
+
+KdTreeKNearestPointIterator::KdTreeKNearestPointIterator() :
+    m_iterator()
+{
+}
+
+KdTreeKNearestPointIterator::KdTreeKNearestPointIterator(limited_priority_queue<IndexSquaredDistance>::iterator iterator) :
+    m_iterator(iterator)
+{
+}
+
+bool KdTreeKNearestPointIterator::operator !=(const KdTreeKNearestPointIterator& other) const
+{
+    return m_iterator != other.m_iterator;
+}
+
+void KdTreeKNearestPointIterator::operator ++()
+{
+    ++m_iterator;
+}
+
+int KdTreeKNearestPointIterator::operator * () const
+{
+    return m_iterator->index;
+}
+
+} // namespace ponca
