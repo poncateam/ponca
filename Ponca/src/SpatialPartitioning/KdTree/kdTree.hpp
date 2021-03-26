c55eb181648e69bc7549301bbfa88148ff9abbe9
GomieroMatthieu <matgomiero@gmail.com>
2021-03-26 15:41:09 +0100

    Auto stash before merge of "spatialpartitionning_kdtreetemplate" and "origin/spatialpartitionning_kdtreetemplate"

--- Ponca/src/SpatialPartitioning/KdTree/kdTree.hpp
+++ Ponca/src/SpatialPartitioning/KdTree/kdTree.hpp
@@ -15,67 +15,118 @@ int KdTree<DataPoint>::node_count() const
 template<class DataPoint>
 int KdTree<DataPoint>::index_count() const
 {
-	return static_cast<int>(m_indices->size());
+	return m_indices.size();
 }
 
 template<class DataPoint>
 int KdTree<DataPoint>::point_count() const
 {
-	return static_cast<int>(m_points->size());
+	return m_points.size();
 }
 
 template<class DataPoint>
 void KdTree<DataPoint>::clear()
 {
-	m_points = nullptr;
-	m_nodes   = nullptr;
-	m_indices = nullptr;
+	m_points.clear();
+	m_nodes.clear();
+	m_indices.clear();
 }
 
+//template<class DataPoint>
+//void KdTree<DataPoint>::build(std::shared_ptr<Vector>& points)
+//{
+//	std::vector<int> ids;
+//	iota(ids.begin(), ids.end(), 0);
+//	this->build( points, ids);
+//}
+//
+//template<class DataPoint>
+//void KdTree<DataPoint>::build(std::shared_ptr<Vector>& points, const std::vector<int>& sampling)
+//{
+//	this->clear();
+//	
+//	m_points = points;
+//	
+//	m_nodes = std::make_shared<std::vector<KdTreeNode<Scalar>>>();
+//	m_nodes->reserve(4 * m_points->size() / m_min_cell_size);
+//	m_nodes->emplace_back();
+//	m_nodes->back().leaf = false;
+//	
+//	m_indices = std::make_shared<std::vector<int>>(sampling);//move operator ou std copy
+//	
+//	int end = static_cast<int>(m_indices->size());
+//	
+//	this->build_rec(0, 0, end, 1);
+//	
+////    PCA_DEBUG_ASSERT(this->valid());
+//}
+//
+//template<class DataPoint>
+//void KdTree<DataPoint>::rebuild(const std::vector<int>& sampling)
+//{
+//	//    PCA_DEBUG_ASSERT(sampling.size() <= m_points->size());
+//
+//    m_nodes->clear();
+//    m_nodes->emplace_back();
+//    m_nodes->back().leaf = false;
+//
+//    *m_indices = sampling;
+//
+//    int end = static_cast<int>(m_indices->size());
+//    this->build_rec(0, 0, end, 1);
+//
+////    PCA_DEBUG_ASSERT(this->valid());
+//}
+
+
 template<class DataPoint>
-void KdTree<DataPoint>::build(std::shared_ptr<Vector>& points)
+template<typename VectorUserContainer>
+inline void KdTree<DataPoint>::build(const VectorUserContainer & points)
 {
 	std::vector<int> ids;
 	iota(ids.begin(), ids.end(), 0);
-	this->build( points, ids);
+	this->build(points, ids);
 }
 
 template<class DataPoint>
-void KdTree<DataPoint>::build(std::shared_ptr<Vector>& points, const std::vector<int>& sampling)
+template<typename VectorUserContainer, typename IndexUserContainer>
+inline void KdTree<DataPoint>::build(const VectorUserContainer & points, const IndexUserContainer & sampling)
 {
 	this->clear();
-	
-	m_points = points;
-	
-	m_nodes = std::make_shared<std::vector<KdTreeNode<Scalar>>>();
-	m_nodes->reserve(4 * m_points->size() / m_min_cell_size);
-	m_nodes->emplace_back();
-	m_nodes->back().leaf = false;
-	
-	m_indices = std::make_shared<std::vector<int>>(sampling);//move operator ou std copy
-	
-	int end = static_cast<int>(m_indices->size());
-	
+
+	//m_points = points;
+	std::copy(points.begin(), points.end(), m_points);
+
+	m_nodes = NodeContainer();
+	m_nodes.reserve(4 * m_points.size() / m_min_cell_size);
+	m_nodes.emplace_back();
+	m_nodes.back().leaf = false;
+
+	m_indices = IndexContainer(sampling);//move operator ou std copy
+
+	int end = m_indices.size();
+
 	this->build_rec(0, 0, end, 1);
-	
-//    PCA_DEBUG_ASSERT(this->valid());
+
+	//    PCA_DEBUG_ASSERT(this->valid());
 }
 
 template<class DataPoint>
-void KdTree<DataPoint>::rebuild(const std::vector<int>& sampling)
+template<typename IndexUserContainer>
+inline void KdTree<DataPoint>::rebuild(const IndexUserContainer & sampling)
 {
 	//    PCA_DEBUG_ASSERT(sampling.size() <= m_points->size());
 
-    m_nodes->clear();
-    m_nodes->emplace_back();
-    m_nodes->back().leaf = false;
+	m_nodes->clear();
+	m_nodes->emplace_back();
+	m_nodes->back().leaf = false;
 
-    *m_indices = sampling;
+	*m_indices = sampling;
 
-    int end = static_cast<int>(m_indices->size());
-    this->build_rec(0, 0, end, 1);
+	int end = static_cast<int>(m_indices->size());
+	this->build_rec(0, 0, end, 1);
 
-//    PCA_DEBUG_ASSERT(this->valid());
+	//    PCA_DEBUG_ASSERT(this->valid());
 }
 
 template<class DataPoint>
@@ -179,9 +230,9 @@ void KdTree<DataPoint>::set_min_cell_size(int min_cell_size)
 template<class DataPoint>
 void KdTree<DataPoint>::build_rec(int node_id, int start, int end, int level)
 {
-	auto& nodes = *m_nodes.get();
-	const auto& points  = *m_points.get();
-	const auto& indices = *m_indices.get();
+	auto& nodes = m_nodes;
+	const auto& points  = m_points;
+	const auto& indices = m_indices;
 	
 	KdTreeNode<Scalar>& node = nodes[node_id];
 	Aabb aabb;
@@ -240,15 +291,15 @@ void KdTree<DataPoint>::build_rec(int node_id, int start, int end, int level)
 template<class DataPoint>
 int KdTree<DataPoint>::partition(int start, int end, int dim, Scalar value)
 {
-	const auto& points = *m_points.get();
-	auto& indices  = *m_indices.get();
+	const auto& points = m_points;
+	auto& indices  = m_indices;
 	
 	auto it = std::partition(indices.begin()+start, indices.begin()+end, [&](int i)
 	{
 	    return points[i][dim] < value;
 	});
 	    
-	auto distance = std::distance(m_indices->begin(), it);
+	auto distance = std::distance(m_indices.begin(), it);
 	
 	return static_cast<int>(distance);
 }
