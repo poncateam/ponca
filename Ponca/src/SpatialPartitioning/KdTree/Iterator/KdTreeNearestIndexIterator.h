476093e1a6e7b6b3294a04840eb52e22e97f4ae4
sachavincent <sacha.vincent@univ-tlse3.fr>
2021-03-26 14:53:32 +0100

    KdTreeNearest & suppression des tests obseletes

--- Ponca/src/SpatialPartitioning/KdTree/Iterator/KdTreeNearestIndexIterator.h
+++ Ponca/src/SpatialPartitioning/KdTree/Iterator/KdTreeNearestIndexIterator.h
@@ -1,12 +1,25 @@
+/*
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 #pragma once
 
-namespace pdpc {
+namespace Ponca {
 
 class KdTreeNearestIndexIterator
 {
 public:
-    KdTreeNearestIndexIterator();
-    KdTreeNearestIndexIterator(int index);
+    KdTreeNearestIndexIterator() :
+        m_index(-1)
+    {
+    }
+
+    KdTreeNearestIndexIterator(int index) :
+        m_index(index)
+    {
+    }
 
 public:
     bool operator !=(const KdTreeNearestIndexIterator& other) const;
@@ -17,4 +30,5 @@ protected:
     int m_index;
 };
 
-} // namespace pdpc
+#include "./KdTreeNearestIndexIterator.hpp"
+} // namespace ponca
