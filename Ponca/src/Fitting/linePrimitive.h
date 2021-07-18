b5d41816090c4ca9101306ec2caaf9528b07d0f7
Aniket Agarwalla <44133710+ani37@users.noreply.github.com>
2021-07-18 22:14:15 +0530

    Update linePrimitive.h

--- Ponca/src/Fitting/linePrimitive.h
+++ Ponca/src/Fitting/linePrimitive.h
@@ -1,9 +1,11 @@
+
 /*
+ Copyright (C) 2021 aniket agarwalla <aniketagarwalla37@gmail.com>
+ 
  This Source Code Form is subject to the terms of the Mozilla Public
  License, v. 2.0. If a copy of the MPL was not distributed with this
  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
-
 
 #pragma onces
 
