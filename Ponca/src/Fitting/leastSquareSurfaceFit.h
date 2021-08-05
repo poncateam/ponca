dbe2986fd92730750b2507024e79be4a7aae60a4
Aniket Agarwalla <44133710+ani37@users.noreply.github.com>
2021-08-05 17:51:28 +0000

    added primitive gradient

--- Ponca/src/Fitting/leastSquareSurfaceFit.h
+++ Ponca/src/Fitting/leastSquareSurfaceFit.h
@@ -14,6 +14,14 @@
 
 #include <Eigen/Eigenvalues>
 
+
+#include "./primitive.h" // PrimitiveBase
+
+#include PONCA_MULTIARCH_INCLUDE_STD(cmath)
+#include PONCA_MULTIARCH_INCLUDE_STD(limits)
+
+#include <Eigen/Core>
+
 namespace Ponca
 {
 
@@ -66,8 +74,10 @@ public:
     // computation data
     Scalar  m_sumW;       /*!< \brief Sum of queries weight.*/
     VectorType m_cog;    /*!< \brief Gravity center of the neighborhood */
+    Eigen::Matrix<Scalar, 9, 1 > cofficient;   /*!< \brief cofficients of the fitting equation */
 
-    Eigen::Matrix<Scalar, 9, 1>  m_right;
+
+    Eigen::Matrix<Scalar, 9, 1>  m_right;   
     Eigen::Matrix<Scalar, 9, 9>  m_cov;     /*!< \brief Covariance matrix */
 
     Solver m_solver;  /*!<\brief Solver used to analyse the covariance matrix */
@@ -81,7 +91,7 @@ public:
     /*! \brief Explicit conversion to LeastSquareSurfaceFit, to access methods potentially hidden by inheritage */
     PONCA_MULTIARCH inline
     LeastSquareSurfaceFit<DataPoint, WFunctor, T>& leastSquareSurfaceFit()
-    { return * static_cast<LeastSquareSurfaceFit<DataPoint, WFunctor, T>*>(this); }
+    { return * static_cast<LeastSquareSurfaceFit<DataPoint, WFunctor, T>*>(this); }    
 
     /**************************************************************************/
     /* Initialization                                                         */
@@ -105,8 +115,6 @@ public:
     /* Results                                                                */
     /**************************************************************************/
 
-    
-
     /*! \brief Reading access to the Solver used to analyse the covariance
       matrix */
     PONCA_MULTIARCH inline const Solver& solver() const { return m_solver; }
@@ -119,192 +127,10 @@ public:
     */
     PONCA_MULTIARCH inline Scalar surfaceVariation() const;
 
-    /*!
-     * \brief Express a point in ambiant space relatively to the tangent surface.
-     * Output vector is: [h, u, v]^T, where u, v are 2d coordinates on the surface,
-     * and h the height of the sample.
-     * \tparam ignoreTranslation must be set to true when passing vectors instead of points
-     */
-    template <bool ignoreTranslation = false>
-    PONCA_MULTIARCH inline VectorType worldToTangentSurface(const VectorType &_q) const;
+    //! \brief Approximation of the scalar field gradient at \f$ \mathbf{q} (not normalized) \f$
+    PONCA_MULTIARCH inline VectorType primitiveGradient (const VectorType& _q) const;
 
-    /*!
-     * \brief Transform a point from the tangent surface [h, u, v]^T to ambiant space
-     * \tparam ignoreTranslation must be set to true when passing vectors instead of points
-     */
-    template <bool ignoreTranslation = false>
-    PONCA_MULTIARCH inline VectorType tangentSurfaceToWorld(const VectorType &_q) const;
-}; //class LeastSquareSurfaceFit
-
-// namespace internal {
-
-// using ::Ponca::internal::FitSpaceDer;
-// using ::Ponca::internal::FitScaleDer;
-
-// /*!
-//     \brief Internal generic class computing the derivatives of covariance surface fits
-//     \inherit Concept::FittingExtensionConcept
-
-//     The differentiation can be done automatically in scale and/or space, by
-//     combining the enum values FitScaleDer and FitSpaceDer in the template
-//     parameter Type.
-
-//     The differenciated values are stored in static arrays. The size of the
-//     arrays is computed with respect to the derivation type (scale and/or space)
-//     and the number of the dimension of the ambiant space.
-//     By convention, the scale derivatives are stored at index 0 when Type
-//     contains at least FitScaleDer. The size of these arrays can be known using
-//     derDimension(), and the differentiation type by isScaleDer() and
-//     isSpaceDer().
-
-//     \ingroup fitting
-//     \internal
-// */
-// template < class DataPoint, class _WFunctor, typename T, int Type>
-// class CovarianceSurfaceDer : public T
-// {
-// private:
-//     typedef T Base; /*!< \brief Generic base type */
-
-
-// protected:
-//     enum
-//     {
-//         Check = Base::PROVIDES_SURFACE,             /*!< \brief Needs surface */
-//         PROVIDES_COVARIANCE_SURFACE_DERIVATIVE,      /*!< \brief Provides derivatives for hyper-surfaces */
-//         PROVIDES_NORMAL_DERIVATIVE
-//     };
-
-//     static const int NbDerivatives   = ((Type & FitScaleDer) ? 1 : 0 ) + ((Type & FitSpaceDer) ? DataPoint::Dim : 0);
-//     static const int DerStorageOrder = (Type & FitSpaceDer) ? Eigen::RowMajor : Eigen::ColMajor;
-
-// public:
-//     typedef typename Base::Scalar     Scalar;     /*!< \brief Inherited scalar type*/
-//     typedef typename Base::VectorType VectorType; /*!< \brief Inherited vector type*/
-//     typedef typename Base::MatrixType MatrixType; /*!< \brief Inherited matrix type*/
-//     typedef typename Base::WFunctor   WFunctor;   /*!< \brief Weight Function*/
-
-//     /*! \brief Static array of scalars with a size adapted to the differentiation type */
-//     typedef Eigen::Matrix<Scalar, DataPoint::Dim, NbDerivatives, DerStorageOrder> VectorArray;
-
-//     /*! \brief Static array of scalars with a size adapted to the differentiation type */
-//     typedef Eigen::Matrix<Scalar, 1, NbDerivatives> ScalarArray;
-// private:
-//     // computation data
-//     ScalarArray m_dSumW;      /*!< \brief Sum of weight derivatives */
-//     MatrixType  m_dCov[NbDerivatives];
-
-//     VectorArray m_dCog;       /*!< \brief Derivatives of the centroid */
-//     VectorArray m_dNormal;    /*!< \brief Derivatives of the hyper-surface normal */
-//     ScalarArray m_dDist;      /*!< \brief Derivatives of the MLS scalar field */
-
-// public:
-
-//     /************************************************************************/
-//     /* Initialization                                                       */
-//     /************************************************************************/
-//     /*! \see Concept::FittingProcedureConcept::init() */
-//     PONCA_MULTIARCH void init(const VectorType &evalPos);
-
-//     /************************************************************************/
-//     /* Processing                                                           */
-//     /************************************************************************/
-//     /*! \see Concept::FittingProcedureConcept::addNeighbor() */
-//     PONCA_MULTIARCH bool addNeighbor(const DataPoint  &nei);
-//     /*! \see Concept::FittingProcedureConcept::finalize() */
-//     PONCA_MULTIARCH FIT_RESULT finalize();
-
-
-//     /**************************************************************************/
-//     /* Use results                                                            */
-//     /**************************************************************************/
-
-//     /*! \brief Returns the derivatives of the scalar field at the evaluation point */
-//     PONCA_MULTIARCH inline ScalarArray dPotential() const { return m_dDist; }
-
-//     /*! \brief Returns the derivatives of the primitive normal */
-//     PONCA_MULTIARCH inline VectorArray dNormal() const { return m_dNormal; }
-
-//     /*! \brief State specified at compilation time to differenciate the fit in scale */
-//     PONCA_MULTIARCH inline bool isScaleDer() const {return bool(Type & FitScaleDer);}
-//     /*! \brief State specified at compilation time to differenciate the fit in space */
-//     PONCA_MULTIARCH inline bool isSpaceDer() const {return bool(Type & FitSpaceDer);}
-//     /*! \brief Number of dimensions used for the differentiation */
-//     PONCA_MULTIARCH inline unsigned int derDimension() const { return NbDerivatives;}
-
-// }; //class CovarianceSurfaceDer
-
-// }// namespace internal
-
-// /*!
-//     \brief Differentiation in scale of the LeastSquareSurfaceFit
-//     \inherit Concept::FittingExtensionConcept
-
-//     Requierement:
-//     \verbatim PROVIDES_COVARIANCE_SURFACE \endverbatim
-//     Provide:
-//     \verbatim PROVIDES_COVARIANCE_SURFACE_SCALE_DERIVATIVE \endverbatim
-
-//     \ingroup fitting
-// */
-// template < class DataPoint, class _WFunctor, typename T>
-// class CovarianceSurfaceScaleDer:public internal::CovarianceSurfaceDer<DataPoint, _WFunctor, T, internal::FitScaleDer>
-// {
-// protected:
-//     /*! \brief Inherited class */
-//     typedef internal::CovarianceSurfaceDer<DataPoint, _WFunctor, T, internal::FitScaleDer> Base;
-//     enum { PROVIDES_COVARIANCE_SURFACE_SCALE_DERIVATIVE, PROVIDES_NORMAL_SCALE_DERIVATIVE };
-// };
-
-
-// /*!
-//     \brief Spatial differentiation of the LeastSquareSurfaceFit
-//     \inherit Concept::FittingExtensionConcept
-
-//     Requierement:
-//     \verbatim PROVIDES_COVARIANCE_SURFACE \endverbatim
-//     Provide:
-//     \verbatim PROVIDES_COVARIANCE_SURFACE_SPACE_DERIVATIVE \endverbatim
-
-//     \ingroup fitting
-// */
-// template < class DataPoint, class _WFunctor, typename T>
-// class CovarianceSurfaceSpaceDer:public internal::CovarianceSurfaceDer<DataPoint, _WFunctor, T, internal::FitSpaceDer>
-// {
-// protected:
-//     /*! \brief Inherited class */
-//     typedef internal::CovarianceSurfaceDer<DataPoint, _WFunctor, T, internal::FitSpaceDer> Base;
-//     enum { PROVIDES_COVARIANCE_SURFACE_SPACE_DERIVATIVE, PROVIDES_NORMAL_SPACE_DERIVATIVE };
-// };
-
-
-// /*!
-//     \brief Differentiation both in scale and space of the LeastSquareSurfaceFit
-//     \inherit Concept::FittingExtensionConcept
-
-//     Requierement:
-//     \verbatim PROVIDES_COVARIANCE_SURFACE \endverbatim
-//     Provide:
-//     \verbatim PROVIDES_COVARIANCE_SURFACE_SCALE_DERIVATIVE
-//     PROVIDES_COVARIANCE_SURFACE_SPACE_DERIVATIVE
-//     \endverbatim
-
-//     \ingroup fitting
-// */
-// template < class DataPoint, class _WFunctor, typename T>
-// class CovarianceSurfaceScaleSpaceDer:public internal::CovarianceSurfaceDer<DataPoint, _WFunctor, T, internal::FitSpaceDer | internal::FitScaleDer>
-// {
-// protected:
-//     /*! \brief Inherited class */
-//     typedef internal::CovarianceSurfaceDer<DataPoint, _WFunctor, T, internal::FitSpaceDer | internal::FitScaleDer> Base;
-//     enum
-//     {
-//         PROVIDES_COVARIANCE_SURFACE_SCALE_DERIVATIVE,
-//         PROVIDES_COVARIANCE_SURFACE_SPACE_DERIVATIVE,
-//         PROVIDES_NORMAL_SCALE_DERIVATIVE,
-//         PROVIDES_NORMAL_SPACE_DERIVATIVE
-//     };
-// };
+}; 
 
 #include "leastSquareSurfaceFit.hpp"
 
--- Ponca/src/Fitting/leastSquareSurfaceFit.hpp
+++ Ponca/src/Fitting/leastSquareSurfaceFit.hpp
@@ -8,7 +8,6 @@
  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
-
 template < class DataPoint, class _WFunctor, typename T>
 void
 LeastSquareSurfaceFit<DataPoint, _WFunctor, T>::init(const VectorType& _evalPos)
@@ -20,7 +19,8 @@ LeastSquareSurfaceFit<DataPoint, _WFunctor, T>::init(const VectorType& _evalPos)
     // Setup fitting internal values
     m_sumW        = Scalar(0.0);
     m_cog         = VectorType::Zero();
-    m_right       = Eigen::Matrix<Scalar, 1, 9> ::Zero();
+    cofficient    = Eigen::Matrix<Scalar, 9, 1> ::Zero();
+    m_right       = Eigen::Matrix<Scalar, 9, 1> ::Zero();
     m_cov         = Eigen::Matrix<Scalar, 9, 9> ::Zero();
 }
 
@@ -29,6 +29,7 @@ bool
 LeastSquareSurfaceFit<DataPoint, _WFunctor, T>::addNeighbor(const DataPoint& _nei)
 {
     VectorType q = _nei.pos() - Base::basisCenter();
+
     // compute weight
     Scalar w = m_w.w(q, _nei);
 
@@ -38,10 +39,12 @@ LeastSquareSurfaceFit<DataPoint, _WFunctor, T>::addNeighbor(const DataPoint& _ne
       m_sumW  += w;
       m_cog  +=  q * w;
 
-     Eigen::Matrix<Scalar, 9, 1> temp;
-     temp << q[0] * q[0], q[1] * q[1] ,q[2] * q[2] , 2* q[0] * q[1] , 2*q[1] * q[2] , 2 * q[0] * q[2] , 2*q;  // temp = [x^2 y^2 z^2 xy yz zx x y z]
-     m_right -= temp;
-     m_cov  +=  temp * temp.transpose(); 
+      // temp = [x^2 y^2 z^2 xy yz zx x y z]
+      Eigen::Matrix<Scalar, 9, 1> temp;
+      temp << q[0] * q[0], q[1] * q[1] ,q[2] * q[2] , 2* q[0] * q[1] , 2*q[1] * q[2] , 2 * q[0] * q[2] , 2*q; 
+     
+      m_right -= temp;
+      m_cov  +=  temp * temp.transpose(); 
 
       ++(Base::m_nbNeighbors);
       
@@ -51,232 +54,46 @@ LeastSquareSurfaceFit<DataPoint, _WFunctor, T>::addNeighbor(const DataPoint& _ne
     return false;
 }
 
-
 template < class DataPoint, class _WFunctor, typename T>
 FIT_RESULT
 LeastSquareSurfaceFit<DataPoint, _WFunctor, T>::finalize ()
 {
 
     // handle specific configurations
-    // With less than 3 neighbors the fitting is undefined
-    if(m_sumW == Scalar(0.) || Base::m_nbNeighbors < 3)
+    // With less than 2 neighbors the fitting is undefined
+    if(m_sumW == Scalar(0.) || Base::m_nbNeighbors < 2)
     {
       Base::resetPrimitive();
       Base::m_eCurrentState = UNDEFINED;
       return Base::m_eCurrentState;
     }
 
-    Eigen::Matrix<Scalar, 9,1 >  result;
-    m_cog = m_cog/m_sumW;
-
-    // Finalize the centroid (still expressed in local basis)
-
-    // // m_cov ====>>>> split
-    // MatrixType A  = m_cov.block(6, 6, 4, 4 );
-    // MatrixType B  = m_cov.block(0, 6, 4, 6 );
-    // MatrixType C  = m_cov.block(0, 0, 6, 6 );
-    // MatrixType BT = m_cov.block(6, 0, 6, 4 );
-      
-    // // [                 ]
-    // // |  C(6*6)  B(4*6) |
-    // // |  B'(6*4) A(4*4) |
-    // // [                 ]   
-
-    // // split the m_cov matrixtype into above matrixtype {C, B, BT, A}
-    // MatrixType AI = A.inverse();// INVERSE(A, AI)
-    // MatrixType Tem1 = B * AI; // MULTMAT(B, AI,TE~l)
-    // MatrixType Tem2 = tem1 * BT; // MULTMAT(TEMP1,BT, TEMP2)
-    // MatrixType Tem2 += C; // ADDMAT(TE~2,C,TE~2)
-    // MatrixType H_inv;
-    // H_inv << 1,0,0,0,0,0,
-    //        0,1,0,0,0,0,
-    //        0,0,1,0,0,0,
-    //        0,0,0,sqrt(2),0,0,
-    //        0,0,0,0,sqrt(2),0,
-    //        0,0,0,0,0,sqrt(2);
-    // Tem2 = H_inv * Tem2;  // MULTMAT(H_CONSTRAINT_INV,TEMP2,TE~2)
-    // m_cov = Tem2 * H_inv;  // MULTMAT(TEMP2,H_CONSTRAINT_INV,TE~2)
-
-    // Center the covariance on the centroid
+    m_cog = m_cog/m_sumW; 
    
- #ifdef __CUDACC__
-     m_solver.computeDirect(m_cov);
-  #else
-    result = m_cov.colPivHouseholderQr().solve(m_right) ;
-  #endif
-   Base::m_eCurrentState = ( m_right.isApprox(m_cov * result)  == true ? STABLE : UNDEFINED );
-   Base::setSurface(result , m_cog);  // RETURN(CALC_QUADRIC_E"QUATION(LAMBDA,EIGEN_VECTORS))
-   return Base::m_eCurrentState;
+    #ifdef __CUDACC__
+      m_solver.computeDirect(m_cov);
+    #else
+      cofficient = m_cov.colPivHouseholderQr().solve(m_right) ;
+    #endif
+
+    Base::m_eCurrentState = ( m_right.isApprox(m_cov * cofficient)  == true ? STABLE : UNDEFINED );
+    Base::setSurface(cofficient , m_cog);  // RETURN(CALC_QUADRIC_E"QUATION(LAMBDA,EIGEN_VECTORS))
+
+    return Base::m_eCurrentState;
 }
 
+template < class DataPoint, class _WFunctor, typename T>
+typename DataPoint::Vectortype
+LeastSquareSurfaceFit<DataPoint, _WFunctor, T>::primitiveGradient( const VectorType &_q ) const
+{
+      MatrixType A;
+      A << cofficient[0], cofficient[5], cofficient[4],
+           cofficient[5], cofficient[1], cofficient[3],
+           cofficient[4], cofficient[3], cofficient[2];
+      
+      VectorType B;
+      B << cofficient[6], cofficient[7], cofficient[8];
+       VectorType result = A * _q + B;
+      return result;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// template < class DataPoint, class _WFunctor, typename T>
-// typename LeastSquareSurfaceFit<DataPoint, _WFunctor, T>::Scalar
-// LeastSquareSurfaceFit<DataPoint, _WFunctor, T>::surfaceVariation () const
-// {
-//     return m_solver.eigenvalues()(0) / m_solver.eigenvalues().mean();
-// }
-
-// template < class DataPoint, class _WFunctor, typename T>
-// template <bool ignoreTranslation>
-// typename LeastSquareSurfaceFit<DataPoint, _WFunctor, T>::VectorType
-// LeastSquareSurfaceFit<DataPoint, _WFunctor, T>::worldToTangentPlane (const VectorType& _q) const
-// {
-//   if (ignoreTranslation)
-//     return m_solver.eigenvectors().transpose() * _q;
-//   else {
-//     // apply rotation and translation to get uv coordinates
-//     return m_solver.eigenvectors().transpose() * (_q - Base::basisCenter());
-//   }
-// }
-
-// template < class DataPoint, class _WFunctor, typename T>
-// template <bool ignoreTranslation>
-// typename LeastSquareSurfaceFit<DataPoint, _WFunctor, T>::VectorType
-// LeastSquareSurfaceFit<DataPoint, _WFunctor, T>::tangentPlaneToWorld (const VectorType& _lq) const
-// {
-//   if (ignoreTranslation)
-//     return m_solver.eigenvectors().transpose().inverse() * _lq;
-//   else {
-//     return m_solver.eigenvectors().transpose().inverse() * _lq + Base::basisCenter();
-//   }
-// }
-
-
-
-// namespace internal
-// {
-
-// template < class DataPoint, class _WFunctor, typename T, int Type>
-// void
-// CovariancePlaneDer<DataPoint, _WFunctor, T, Type>::init(const VectorType& _evalPos)
-// {
-//     Base::init(_evalPos);
-
-//     m_dCog   = VectorArray::Zero();
-//     m_dSumW  = ScalarArray::Zero();
-//     for(int k=0; k<NbDerivatives; ++k)
-//       m_dCov[k].setZero();
-// }
-
-
-// template < class DataPoint, class _WFunctor, typename T, int Type>
-// bool
-// CovariancePlaneDer<DataPoint, _WFunctor, T, Type>::addNeighbor(const DataPoint  &_nei)
-// {
-//     bool bResult = Base::addNeighbor(_nei);
-
-//     if(bResult)
-//     {
-//         int spaceId = (Type & FitScaleDer) ? 1 : 0;
-
-//         ScalarArray dw;
-
-//         // centered basis
-//         VectorType q = _nei.pos()-Base::basisCenter();
-
-//         // compute weight
-//         if (Type & FitScaleDer)
-//             dw[0] = Base::m_w.scaledw(q, _nei);
-
-//         if (Type & FitSpaceDer)
-//             dw.template segment<int(DataPoint::Dim)>(spaceId) = -Base::m_w.spacedw(q, _nei).transpose();
-
-//         m_dSumW += dw;
-//         m_dCog  += q * dw;
-//         for(int k=0; k<NbDerivatives; ++k)
-//           m_dCov[k]  += dw[k] * q * q.transpose();
-
-//         return true;
-//     }
-
-//     return false;
-// }
-
-
-// template < class DataPoint, class _WFunctor, typename T, int Type>
-// FIT_RESULT
-// CovariancePlaneDer<DataPoint, _WFunctor, T, Type>::finalize()
-// {
-//     PONCA_MULTIARCH_STD_MATH(sqrt);
-
-//     Base::finalize();
-//     // Test if base finalize end on a viable case (stable / unstable)
-//     if (this->isReady())
-//     {
-//       // pre-compute shifted eigenvalues to apply the pseudo inverse of C - lambda_0 I
-//       Scalar epsilon          = Scalar(2) * Eigen::NumTraits<Scalar>::epsilon();
-//       Scalar consider_as_zero = Scalar(2) * std::numeric_limits<Scalar>::denorm_min();
-//       Eigen::Matrix<Scalar,2,1> shifted_eivals = Base::m_solver.eigenvalues().template tail<2>().array() - Base::m_solver.eigenvalues()(0);
-//       if(shifted_eivals(0) < consider_as_zero || shifted_eivals(0) < epsilon * shifted_eivals(1)) shifted_eivals(0) = 0;
-//       if(shifted_eivals(1) < consider_as_zero) shifted_eivals(1) = 0;
-
-//       for(int k=0; k<NbDerivatives; ++k)
-//       {
-//         // Finalize the computation of dCov.
-//         m_dCov[k] = m_dCov[k]
-//                   - Base::m_cog * m_dCog.col(k).transpose()
-//                   - m_dCog.col(k) * Base::m_cog.transpose()
-//                   + m_dSumW[k] * Base::m_cog * Base::m_cog.transpose();
-
-//         // apply normalization by sumW:
-//         m_dCog.col(k) = (m_dCog.col(k) - m_dSumW(k) * Base::m_cog) / Base::m_sumW;
-
-//         VectorType normal = Base::primitiveGradient();
-//         // The derivative of 'normal' is the derivative of the smallest eigenvector.
-//         // Since the covariance matrix is real and symmetric, it is equal to:
-//         //    n' = - (C - lambda_0 I)^+ C' n
-//         // Where ^+ denotes the pseudo-inverse.
-//         // Since we already performed the eigenvalue decomposition of the matrix C,
-//         // we can directly apply the pseudo inverse by observing that:
-//         //    (C - lambda_0 I) = V (L - lambda_0 I) V^T
-//         // where V is the eigenvector matrix, and L the eigenvalue diagonal matrix.
-//         Eigen::Matrix<Scalar,2,1> z = - Base::m_solver.eigenvectors().template rightCols<2>().transpose() * (m_dCov[k] * normal);
-//         if(shifted_eivals(0)>0) z(0) /= shifted_eivals(0);
-//         if(shifted_eivals(1)>0) z(1) /= shifted_eivals(1);
-//         m_dNormal.col(k) = Base::m_solver.eigenvectors().template rightCols<2>() * z;
-
-//         VectorType dDiff = -m_dCog.col(k);
-//         if(k>0 || !isScaleDer())
-//           dDiff(isScaleDer() ? k-1 : k) += 1;
-//         m_dDist(k) = m_dNormal.col(k).dot(Base::m_cog) + normal.dot(dDiff);
-
-//         // \fixme we shouldn't need this normalization, however currently the derivatives are overestimated by a factor 2
-//         m_dNormal /= Scalar(2.);
-//       }
-//     }
-
-//     return Base::m_eCurrentState;
-// }
-
-// }// namespace internal
+}
