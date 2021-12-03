f4cc8bbc26c50709cb2a7300a73cd955da788114
Nicolas Mellado <nmellado0@gmail.com>
2021-12-03 10:10:50 +0100

    [cmake] add missing file

--- cmake/PoncaConfigureFitting.cmake
+++ cmake/PoncaConfigureFitting.cmake
@@ -12,6 +12,7 @@ set(ponca_Fitting_INCLUDE
     "${PONCA_src_ROOT}/Ponca/src/Fitting/curvatureEstimation.hpp"
     "${PONCA_src_ROOT}/Ponca/src/Fitting/curvature.h"
     "${PONCA_src_ROOT}/Ponca/src/Fitting/curvature.hpp"
+    "${PONCA_src_ROOT}/Ponca/src/Fitting/dryFit.h"
     "${PONCA_src_ROOT}/Ponca/src/Fitting/enums.h"
     "${PONCA_src_ROOT}/Ponca/src/Fitting/gls.h"
     "${PONCA_src_ROOT}/Ponca/src/Fitting/gls.hpp"
