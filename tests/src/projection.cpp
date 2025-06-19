15a5a2a9d00b56947dd79df9983f315f03d9c762
fauberva <florian.auberval@irit.fr>
2025-06-19 15:31:16 +0200

    [projection] Updated setWeight and init in the tests

--- tests/src/projection.cpp
+++ tests/src/projection.cpp
@@ -57,7 +57,7 @@ void testFunction()
     Scalar epsilon = Scalar(0.001); // We need a lesser precision for this test to pass
 
     Fit fit;
-    fit.setWeightFunc(WeightFunc(analysisScale, center));
+    fit.setWeightFunc(WeightFunc(center, analysisScale));
 
     for(int i = 0; i < nbPointsFit; ++i)
     {
