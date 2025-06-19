ad3c8b6fd2c1309e0c712a7bbb454025ed4b0cff
fauberva <florian.auberval@irit.fr>
2025-06-19 15:31:07 +0200

    [normal_sphere] Updated setWeight and init in the tests

--- tests/src/normal_sphere.cpp
+++ tests/src/normal_sphere.cpp
@@ -47,8 +47,7 @@ void testFunction(bool _bAddPositionNoise = false, bool _bAddNormalNoise = false
     for(int i = 0; i < int(vectorPoints.size()); ++i)
     {
         Fit fit;
-        fit.setWeightFunc(WeightFunc(analysisScale));
-        fit.init(vectorPoints[i].pos());
+        fit.setWeightFunc(WeightFunc(vectorPoints[i].pos(), analysisScale));
         fit.compute(vectorPoints);
 
         if(fit.isStable())
