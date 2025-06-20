28a4120f6eae1ece3e090dcaff40db4ad72bce12
fauberva <florian.auberval@irit.fr>
2025-06-20 10:38:02 +0200

    [curvature_sphere] Added back the init for consistency

--- tests/src/curvature_sphere.cpp
+++ tests/src/curvature_sphere.cpp
@@ -54,6 +54,7 @@ void testFunction(bool _bAddPositionNoise = false, bool _bAddNormalNoise = false
 
         Fit fit;
         fit.setWeightFunc(WeightFunc(vectorPoints[i].pos(), analysisScale));
+        fit.init();
         fit.compute(vectorPoints);
 
         if( fit.isStable() )
