81d752b550c0e3bb56760709251d35fb43ce8dee
fauberva <florian.auberval@irit.fr>
2025-06-20 10:37:23 +0200

    [dnormal_sphere] Added back the init for consistency

--- tests/src/dnormal_sphere.cpp
+++ tests/src/dnormal_sphere.cpp
@@ -52,6 +52,7 @@ void testFunction(bool _bAddPositionNoise = false, bool _bAddNormalNoise = false
     {
         Fit fit;
         fit.setWeightFunc(WeightFunc(vectorPoints[i].pos(), analysisScale));
+        fit.init();
         fit.compute(vectorPoints);
 
         if(fit.isStable())
