f5c5516df04b25543d469d191af15ac667f9817b
fauberva <florian.auberval@irit.fr>
2025-07-03 15:53:27 +0200

    [fit] added a macro that defines the fit types enum

---
+++ Ponca/src/Fitting/Estimators/defineEnum.h
@@ -0,0 +1,31 @@
+#pragma once
+
+// The different fit types are compiled depending on if they are defined through this macro or not
+#define ENUM_FITS \
+ENUM_FIT(ASO) \
+/*
+ENUM_FIT(APSS) \
+ENUM_FIT(PSS) \
+ENUM_FIT(Ellipsoid) \
+ENUM_FIT(ShapeOperator) \
+ENUM_FIT(PCA) \
+*/
+
+////////////////////////////////////////////////////////////
+
+namespace Estimators {
+
+    /*! \brief Enum of every available fit type */
+    enum class FitType {
+#define ENUM_FIT(type) type,
+        ENUM_FITS
+#undef ENUM_FIT
+    };
+
+    /*! \brief Count the number of different available fit types */
+    inline constexpr int NUMBER_OF_FIT_TYPES = 0
+#define ENUM_FIT(type) +1
+        ENUM_FITS
+#undef ENUM_FIT
+    ;
+}
