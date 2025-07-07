23c145021af09ed00e94c5d78c7d2c9dd4bb8652
fauberva <florian.auberval@irit.fr>
2025-07-07 09:57:06 +0200

    [fit] Moving adapters structs to the ponca namespace

--- Ponca/src/Fitting/Estimators/baseType.h
+++ Ponca/src/Fitting/Estimators/baseType.h
@@ -3,32 +3,33 @@
 #include <Eigen/Core>
 #include <Ponca/SpatialPartitioning>
 
+namespace Ponca {
+    /// Map a block to a Ponca point
+    template <typename _Scalar>
+    struct BlockPointAdapter {
+    public:
+        enum {Dim = 3};
+        using Scalar     = _Scalar;
+        using VectorType = Eigen::Matrix<Scalar, Dim, 1>;
+        using MatrixType = Eigen::Matrix<Scalar, Dim, Dim>;
 
-/// Map a block to a Ponca point
-template <typename _Scalar>
-struct BlockPointAdapter {
-public:
-    enum {Dim = 3};
-    using Scalar     = _Scalar;
-    using VectorType = Eigen::Matrix<Scalar, Dim, 1>;
-    using MatrixType = Eigen::Matrix<Scalar, Dim, Dim>;
+        /// \brief Map a vector as ponca Point
+        PONCA_MULTIARCH inline BlockPointAdapter(VectorType v, VectorType n)
+                : m_pos (v), m_nor (n) {}
 
-    /// \brief Map a vector as ponca Point
-    PONCA_MULTIARCH inline BlockPointAdapter(VectorType v, VectorType n)
-            : m_pos (v), m_nor (n) {}
+        PONCA_MULTIARCH inline VectorType pos()    const { return m_pos; }
+        PONCA_MULTIARCH inline VectorType normal() const { return m_nor; }
 
-    PONCA_MULTIARCH inline VectorType pos()    const { return m_pos; }
-    PONCA_MULTIARCH inline VectorType normal() const { return m_nor; }
+        PONCA_MULTIARCH inline BlockPointAdapter& operator=(const BlockPointAdapter& other) = default;
 
-    PONCA_MULTIARCH inline BlockPointAdapter& operator=(const BlockPointAdapter& other) = default;
+    private:
+        VectorType m_pos;
+        VectorType m_nor;
+    };
 
-private:
-    VectorType m_pos;
-    VectorType m_nor;
-};
-
-using Scalar = float;
-using PPAdapter = BlockPointAdapter<Scalar>;
-using VectorType = Eigen::Matrix<Scalar, 3, 1>;
-using SampleMatrixType = Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>;
-using SampleVectorType = Eigen::Matrix<Scalar, Eigen::Dynamic, 1>;
+    using Scalar = float;
+    using PPAdapter = BlockPointAdapter<Scalar>;
+    using VectorType = Eigen::Matrix<Scalar, 3, 1>;
+    using SampleMatrixType = Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>;
+    using SampleVectorType = Eigen::Matrix<Scalar, Eigen::Dynamic, 1>;
+}
