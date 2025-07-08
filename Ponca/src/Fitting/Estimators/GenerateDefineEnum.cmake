a868fa27783c1849cbafec0415ca5871754d424c
fauberva <florian.auberval@irit.fr>
2025-07-08 10:32:49 +0200

    [fit] Removed the generated header file from the source dir `configure_file` now outputs the generated header to the generated directory

--- Ponca/src/Fitting/Estimators/GenerateDefineEnum.cmake
+++ Ponca/src/Fitting/Estimators/GenerateDefineEnum.cmake
@@ -2,13 +2,19 @@ set(ENUM_FITS "PSS ASO APSS PCA Sphere" CACHE STRING "The fit types that the est
 
 message(STATUS "Compiling Estimator Factory for the following FIT TYPES : ${ENUM_FITS}")
 
-
-
 # Transform the list into lines like: ENUM_FIT(name)
 # This assumes names are space-separated
 string(REPLACE " " ")\\\nENUM_FIT(" ENUM_FITS_CONTENT "${ENUM_FITS}")
 set(ENUM_FITS_CONTENT "ENUM_FIT(${ENUM_FITS_CONTENT})")
 
+set(GENERATED_HEADER_DIR "${CMAKE_BINARY_DIR}/generated")
+file(MAKE_DIRECTORY ${GENERATED_HEADER_DIR})
+
 # Configure header file
-configure_file(${CMAKE_CURRENT_SOURCE_DIR}/Ponca/src/Fitting/Estimators/defineEnum.h.in
-        ${CMAKE_CURRENT_SOURCE_DIR}/Ponca/src/Fitting/Estimators/defineEnum.h @ONLY)
+configure_file(
+        ${CMAKE_CURRENT_SOURCE_DIR}/Ponca/src/Fitting/Estimators/defineEnum.h.in
+        ${GENERATED_HEADER_DIR}/defineEnum.h
+        @ONLY
+)
+
+include_directories(${GENERATED_HEADER_DIR})
