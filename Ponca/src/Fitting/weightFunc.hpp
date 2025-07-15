debf4f3ee44cf823639341f0d58db81d12c50839
Nicolas Mellado <nmellado0@gmail.com>
2025-07-15 16:45:36 +0200

    [Fitting] Update Weighting function to separate weights and local basis

--- Ponca/src/Fitting/weightFunc.hpp
+++ Ponca/src/Fitting/weightFunc.hpp
@@ -4,52 +4,67 @@
  file, You can obtain one at http://mozilla.org/MPL/2.0/. 
 */
 
+#pragma once
+#include "weightFunc.h"
 
-template <class DataPoint, class _NFilter>
-typename DistWeightFunc<DataPoint, _NFilter>::VectorType
-DistWeightFunc<DataPoint, _NFilter>::convertToGlobalBasis(const VectorType& _q) const
+template<class DataPoint, bool _local>
+typename NeighborhoodFrameBase<DataPoint, _local>::VectorType
+NeighborhoodFrameBase<DataPoint, _local>::convertToGlobalBasisInternal(const VectorType& _q) const
 {
     return _q + m_p;
 }
 
-template <class DataPoint, class _NFilter>
-typename DistWeightFunc<DataPoint, _NFilter>::VectorType
-DistWeightFunc<DataPoint, _NFilter>::convertToLocalBasis(const VectorType& _q) const
+template<class DataPoint, bool _local>
+typename NeighborhoodFrameBase<DataPoint, _local>::VectorType
+NeighborhoodFrameBase<DataPoint, _local>::convertToLocalBasisInternal(const VectorType& _q) const
 {
     return _q - m_p;
 }
 
-template <class DataPoint, class _NFilter>
-typename DistWeightFunc<DataPoint, _NFilter>::WeightReturnType
-DistWeightFunc<DataPoint, _NFilter>::w( const VectorType& _q,
-					                        const DataPoint&) const
+template<class DataPoint, bool _local>
+typename NeighborhoodFrameBase<DataPoint, _local>::VectorType
+NeighborhoodFrameBase<DataPoint, _local>::convertToGlobalBasis(const VectorType& _q) const
 {
-    VectorType q = convertToLocalBasis(_q);
+    return _local ? convertToGlobalBasisInternal(_q) : _q;
+}
+
+template<class DataPoint, bool _local>
+typename NeighborhoodFrameBase<DataPoint, _local>::VectorType
+NeighborhoodFrameBase<DataPoint, _local>::convertToLocalBasis(const VectorType& _q) const
+{
+    return _local ? convertToLocalBasisInternal(_q) : _q;
+}
+
+template <class DataPoint, class WeightKernel, bool _CenterCoordinates>
+typename DistWeightFuncBase<DataPoint, WeightKernel, _CenterCoordinates>::WeightReturnType
+DistWeightFuncBase<DataPoint, WeightKernel, _CenterCoordinates>::operator()( const DataPoint& _q) const
+{
+    VectorType q = NeighborhoodFrame::convertToLocalBasisInternal(_q.pos());
     Scalar d  = q.norm();
     return { (d <= m_t) ? m_wk.f(d/m_t) : Scalar(0.), q };
 }
 
-template <class DataPoint, class _NFilter>
-typename DistWeightFunc<DataPoint, _NFilter>::VectorType
-DistWeightFunc<DataPoint, _NFilter>::spacedw(   const VectorType& _q,
-						                            const DataPoint&) const
+template <class DataPoint, class WeightKernel, bool _CenterCoordinates>
+typename DistWeightFuncBase<DataPoint, WeightKernel, _CenterCoordinates>::VectorType
+DistWeightFuncBase<DataPoint, WeightKernel, _CenterCoordinates>::spacedw( const VectorType& _q,
+						                                                  const DataPoint&) const
 {
-    static_assert(_NFilter::isDValid, "First order derivatives are required");
+    static_assert(WeightKernel::isDValid, "First order derivatives are required");
     VectorType result = VectorType::Zero();
-    VectorType q = convertToLocalBasis(_q);
+    VectorType q = NeighborhoodFrame::convertToLocalBasisInternal(_q);
     Scalar d = q.norm();
     if (d <= m_t && d != Scalar(0.)) result = (q / (d * m_t)) * m_wk.df(d/m_t);
     return result;
 }
 
-template <class DataPoint, class WeightKernel>
-typename DistWeightFunc<DataPoint, WeightKernel>::MatrixType
-DistWeightFunc<DataPoint, WeightKernel>::spaced2w(   const VectorType& _q,
-                                                     const DataPoint&) const
+template <class DataPoint, class WeightKernel, bool _CenterCoordinates>
+typename DistWeightFuncBase<DataPoint, WeightKernel, _CenterCoordinates>::MatrixType
+DistWeightFuncBase<DataPoint, WeightKernel, _CenterCoordinates>::spaced2w( const VectorType& _q,
+                                                                           const DataPoint&) const
 {
     static_assert(WeightKernel::isDDValid, "Second order derivatives are required");
     MatrixType result = MatrixType::Zero();
-    VectorType q = convertToLocalBasis(_q);
+    VectorType q = NeighborhoodFrame::convertToLocalBasisInternal(_q);
     Scalar d = q.norm();
     if (d <= m_t && d != Scalar(0.))
     {
@@ -61,36 +76,36 @@ DistWeightFunc<DataPoint, WeightKernel>::spaced2w(   const VectorType& _q,
     return result;
 }
 
-template <class DataPoint, class WeightKernel>
-typename DistWeightFunc<DataPoint, WeightKernel>::Scalar
-DistWeightFunc<DataPoint, WeightKernel>::scaledw(   const VectorType& _q, 
-						                            const DataPoint&) const
+template <class DataPoint, class WeightKernel, bool _CenterCoordinates>
+typename DistWeightFuncBase<DataPoint, WeightKernel, _CenterCoordinates>::Scalar
+DistWeightFuncBase<DataPoint, WeightKernel, _CenterCoordinates>::scaledw( const VectorType& _q,
+                                                                          const DataPoint&) const
 {
     static_assert(WeightKernel::isDValid, "First order derivatives are required");
-    Scalar d  = convertToLocalBasis(_q).norm();
+    Scalar d  = NeighborhoodFrame::convertToLocalBasisInternal(_q).norm();
     return (d <= m_t) ? Scalar( - d*m_wk.df(d/m_t)/(m_t*m_t) ) : Scalar(0.);
 }
 
-template <class DataPoint, class WeightKernel>
-typename DistWeightFunc<DataPoint, WeightKernel>::Scalar
-DistWeightFunc<DataPoint, WeightKernel>::scaled2w(   const VectorType& _q,
-                                                     const DataPoint&) const
+template <class DataPoint, class WeightKernel, bool _CenterCoordinates>
+typename DistWeightFuncBase<DataPoint, WeightKernel, _CenterCoordinates>::Scalar
+DistWeightFuncBase<DataPoint, WeightKernel, _CenterCoordinates>::scaled2w( const VectorType& _q,
+                                                                           const DataPoint&) const
 {
     static_assert(WeightKernel::isDDValid, "Second order derivatives are required");
-    Scalar d  = convertToLocalBasis(_q).norm();
+    Scalar d  = NeighborhoodFrame::convertToLocalBasisInternal(_q).norm();
     return (d <= m_t) ? Scalar(Scalar(2.)*d/(m_t*m_t*m_t)*m_wk.df(d/m_t) +
                                d*d/(m_t*m_t*m_t*m_t)*m_wk.ddf(d/m_t)) :
                         Scalar(0.);
 }
 
-template <class DataPoint, class WeightKernel>
-typename DistWeightFunc<DataPoint, WeightKernel>::VectorType
-DistWeightFunc<DataPoint, WeightKernel>::scaleSpaced2w(   const VectorType& _q,
-                                                          const DataPoint&) const
+template <class DataPoint, class WeightKernel, bool _CenterCoordinates>
+typename DistWeightFuncBase<DataPoint, WeightKernel, _CenterCoordinates>::VectorType
+DistWeightFuncBase<DataPoint, WeightKernel, _CenterCoordinates>::scaleSpaced2w( const VectorType& _q,
+                                                                                const DataPoint&) const
 {
     static_assert(WeightKernel::isDDValid, "Second order derivatives are required");
     VectorType result = VectorType::Zero();
-    VectorType q = convertToLocalBasis(_q);
+    VectorType q = NeighborhoodFrame::convertToLocalBasisInternal(_q);
     Scalar d = q.norm();
     if (d <= m_t && d != Scalar(0.))
         result = -q/(m_t*m_t)*(m_wk.df(d/m_t)/d + m_wk.ddf(d/m_t)/m_t);
