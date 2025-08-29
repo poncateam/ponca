b791e659b7e28e0536644bc73c7467154a023f3f
fauberva <florian.auberval@irit.fr>
2025-08-29 14:39:12 +0200

    [ci] Made the fit comparision it's own function in projection test Brings back the old test function that we can use once the global neighborhood filter is fixed.

--- tests/src/projection.cpp
+++ tests/src/projection.cpp
@@ -28,8 +28,96 @@
 using namespace std;
 using namespace Ponca;
 
+/*
+ * Test the OrientedSphereFit on a paraboloid using a given neighborhood filter
+ */
+template<typename DataPoint, typename NF>
+void testFunction(typename DataPoint::Scalar lowPrecisionEpsilon = typename DataPoint::Scalar(0.001)) // Lesser precision for the paraboloid test
+{
+    // Define related structure
+    typedef typename DataPoint::Scalar Scalar;
+    typedef typename DataPoint::VectorType VectorType;
+    typedef Basket<DataPoint, NF, OrientedSphereFit> Fit;
+
+    //generate samples
+    int nbPoints = Eigen::internal::random<int>(100, 1000);
+    int nbPointsFit = 50;
+
+    // equal probability of having a plane or a random quadric
+    VectorType coeff = 5 * VectorType::Random();
+    if(Eigen::internal::random<Scalar>(0., 1.) < Scalar(0.5))
+    {
+        coeff = VectorType::Zero();
+    }
+    // avoid saddles, which can cause issues with the sphere fitting
+    coeff.y() = std::copysign(coeff.y(), coeff.x());
+
+    Scalar width = Eigen::internal::random<Scalar>(1., 10.);
+    VectorType center = 1000 * VectorType::Random();
+
+    Scalar zmax = std::abs((coeff[0] + coeff[1]) * width*width);
+    Scalar analysisScale = std::sqrt(zmax*zmax + width*width);
+
+    Fit fit;
+    fit.setNeighborFilter(NF(center, analysisScale));
+    fit.init();
+
+    for(int i = 0; i < nbPointsFit; ++i)
+    {
+        DataPoint p = getPointOnParaboloid<DataPoint>(coeff.x(),
+                                                      coeff.y(),
+                                                      width,
+                                                      false);           // noise
+        p.pos() += center;
+
+        fit.addNeighbor(p);
+    }
+
+    if(fit.isStable())
+    {
+        std::vector<VectorType> samples (nbPoints);
+        for (int i = 0; i < nbPoints; ++i)
+        {
+            VectorType p = center + analysisScale * VectorType::Random();
+            samples[i] = p;
+
+            // check that the projected point is on the surface
+            VERIFY( std::abs(fit.potential(fit.project(p))) < lowPrecisionEpsilon );
+        }
+
+        // check if direct projection gives same or better result than descent projection.
+        for( const auto& p: samples )
+        {
+            VectorType res1 = fit.project( p );
+            VectorType res2 = fit.projectDescent( p, 1000 ); // force high number of iterations
+            VERIFY( res1.isApprox( res2, lowPrecisionEpsilon ));
+        }
+
+        // Disable this test: not true with apple-clang 12.
+#ifdef COMPARE_PROJECTION_TIMINGS
+        auto start1 = std::chrono::system_clock::now();
+        for( const auto& p: samples )
+          fit.project( p );
+        auto end1 = std::chrono::system_clock::now();
+
+        auto start2 = std::chrono::system_clock::now();
+        for( const auto& p: samples )
+          fit.projectDescent( p );
+        auto end2 = std::chrono::system_clock::now();
+
+        std::chrono::duration<double> elapsed_seconds1 = end1-start1;
+        std::chrono::duration<double> elapsed_seconds2 = end2-start2;
+        std::cout << "Default: " << elapsed_seconds1.count() << " Descent: " << elapsed_seconds2.count() << "s\n";
+        VERIFY( elapsed_seconds1 <= elapsed_seconds2 );
+#endif
+    }
+}
+
+/*
+ * Compare two neighborhood filter using the OrientedSphereFit on the paraboloid test
+ */
 template<typename DataPoint, typename NF, typename NFG>
-void testFunction(typename DataPoint::Scalar lowPrecisionEpsilon = typename DataPoint::Scalar(0.1)) // Lesser precision for the paraboloid test
+void compareNF(typename DataPoint::Scalar lowPrecisionEpsilon = typename DataPoint::Scalar(0.1)) // Lesser precision for the paraboloid test
 {
     // Define related structure
     typedef typename DataPoint::Scalar Scalar;
@@ -37,7 +125,7 @@ void testFunction(typename DataPoint::Scalar lowPrecisionEpsilon = typename Data
     typedef Basket<DataPoint, NF, OrientedSphereFit> Fit;
     typedef Basket<DataPoint, NFG, OrientedSphereFit> FitG;
 
-    //generate samples
+    // generate samples
     int nbPoints = Eigen::internal::random<int>(100, 1000);
     int nbPointsFit = 50;
 
@@ -73,7 +161,6 @@ void testFunction(typename DataPoint::Scalar lowPrecisionEpsilon = typename Data
 
         fit.addNeighbor(p);
         fitG.addNeighbor(DataPoint(p.pos(), p.normal()));
-        // fitG.addNeighbor(DataPoint(p.pos(), p.normal()));
     }
     std::cout << "------------------------------------------------ IN fit.finalize() ------------------------------------------------ " << std::endl;
     fit.finalize();
@@ -121,28 +208,7 @@ void testFunction(typename DataPoint::Scalar lowPrecisionEpsilon = typename Data
             VectorType res2G = fitG.projectDescent( p, 1000 ); // force high number of iterations
             VERIFY( res1G.isApprox( res2G, lowPrecisionEpsilon ));
         }
-
-        // Disable this test: not true with apple-clang 12.
-#ifdef COMPARE_PROJECTION_TIMINGS
-        auto start1 = std::chrono::system_clock::now();
-        for( const auto& p: samples )
-          fit.project( p );
-        auto end1 = std::chrono::system_clock::now();
-
-        auto start2 = std::chrono::system_clock::now();
-        for( const auto& p: samples )
-          fit.projectDescent( p );
-        auto end2 = std::chrono::system_clock::now();
-
-        std::chrono::duration<double> elapsed_seconds1 = end1-start1;
-        std::chrono::duration<double> elapsed_seconds2 = end2-start2;
-        std::cout << "Default: " << elapsed_seconds1.count() << " Descent: " << elapsed_seconds2.count() << "s\n";
-        VERIFY( elapsed_seconds1 <= elapsed_seconds2 );
-#endif
-
     }
-
-
 }
 
 template<typename Scalar, int Dim>
@@ -158,10 +224,10 @@ void callSubTests()
     cout << "Testing with parabola..." << endl;
     for(int i = 0; i < g_repeat; ++i)
     {
-        CALL_SUBTEST(( testFunction<Point, WeightSmoothFunc, WeightSmoothFuncGlobal>(0.1) ));
-        CALL_SUBTEST(( testFunction<Point, WeightConstantFunc, WeightConstantFuncGlobal>(0.1) ));
-        // CALL_SUBTEST(( testFunction<Point, WeightSmoothFuncGlobal>(0.1) ));
-        // CALL_SUBTEST(( testFunction<Point, WeightConstantFuncGlobal>(0.1) ));
+        CALL_SUBTEST(( compareNF<Point, WeightSmoothFunc, WeightSmoothFuncGlobal>(0.1) ));
+        CALL_SUBTEST(( compareNF<Point, WeightConstantFunc, WeightConstantFuncGlobal>(0.1) ));
+        // CALL_SUBTEST(( testFunction<Point, WeightSmoothFuncGlobal>() ));
+        // CALL_SUBTEST(( testFunction<Point, WeightConstantFuncGlobal>() ));
     }
     cout << "Ok!" << endl;
 }
