a47d0af6686b412ac16b89de5943ab8af569436f
fauberva <florian.auberval@irit.fr>
2025-09-01 16:27:16 +0200

    [fitting] Added DataPoint to template arguments for genericity Instead of using PPAdapter

--- Ponca/src/Fitting/Estimators/defineEstimators.h
+++ Ponca/src/Fitting/Estimators/defineEstimators.h
@@ -2,12 +2,11 @@
 
 #include "poncaKernels.h"
 #include "estimator.h"
-#include "baseType.h"
 
 namespace Ponca::Estimators {
     /// Distance to PCA plane : Mean curvature
-    template <typename WeightFunc>
-    using Fit_PCA = Basket<PPAdapter, WeightFunc, Ponca::CovariancePlaneFit>;
+    template <typename WeightFunc, typename DataPoint>
+    using Fit_PCA = Basket<DataPoint, WeightFunc, Ponca::CovariancePlaneFit>;
 
     /// Distance to PCA plane : Curvature Tensor
     // template <typename WeightFunc>
@@ -18,16 +17,16 @@ namespace Ponca::Estimators {
     //             Ponca::CurvatureEstimatorBase, Ponca::NormalDerivativesCurvatureEstimator>;
 
     /// Point Set Surfaces : Curvature Tensor
-    template <typename WeightFunc>
+    template <typename WeightFunc, typename DataPoint>
     using Fit_PSS = Ponca::BasketDiff<
-            Fit_PCA<WeightFunc>,
+            Fit_PCA<WeightFunc, DataPoint>,
             Ponca::DiffType::FitSpaceDer,
             Ponca::CovariancePlaneDer,
     Ponca::CurvatureEstimatorBase, Ponca::NormalDerivativesCurvatureEstimator>;
 
     /// Algebraic Point Set Surfaces : Mean curvature
-    template <typename WeightFunc>
-    using Fit_APSS = Ponca::Basket<PPAdapter, WeightFunc, Ponca::OrientedSphereFit>;
+    template <typename WeightFunc, typename DataPoint>
+    using Fit_APSS = Ponca::Basket<DataPoint, WeightFunc, Ponca::OrientedSphereFit>;
 
     /// Algebraic Point Set Surfaces : Curvature Tensor
     // template <typename WeightFunc>
@@ -39,16 +38,16 @@ namespace Ponca::Estimators {
 
 
     /// Algebraic Shape Operator : Curvature Tensor
-    template <typename WeightFunc>
+    template <typename WeightFunc, typename DataPoint>
     using Fit_ASO = Ponca::BasketDiff<
-                Fit_APSS<WeightFunc>,
+                Fit_APSS<WeightFunc, DataPoint>,
                 Ponca::DiffType::FitSpaceDer,
                 Ponca::OrientedSphereDer, Ponca::MlsSphereFitDer,
                 Ponca::CurvatureEstimatorBase, Ponca::NormalDerivativesCurvatureEstimator>;
 
     /// Sphere : Mean curvature
-    template <typename WeightFunc>
-    using Fit_Sphere = Ponca::Basket<PPAdapter, WeightFunc, Ponca::SphereFit>;
+    template <typename WeightFunc, typename DataPoint>
+    using Fit_Sphere = Ponca::Basket<DataPoint, WeightFunc, Ponca::SphereFit>;
     /// Sphere : Curvature Tensor
     // template <typename WeightFunc>
     // using Fit_Sphere_Diff = Ponca::BasketDiff<
