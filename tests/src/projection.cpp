a637cebb2bb39b7564728f00fc1143fa57b5fe86
fauberva <florian.auberval@irit.fr>
2025-09-04 15:50:55 +0200

    [ci] Removes the weight comparision debug test

--- tests/src/projection.cpp
+++ tests/src/projection.cpp
@@ -116,104 +116,6 @@ void testFunction(typename DataPoint::Scalar lowPrecisionEpsilon = typename Data
     }
 }
 
-/*
- * Compare two neighborhood filter using the OrientedSphereFit on the paraboloid test
- */
-template<typename DataPoint, typename NF, typename NFG>
-void compareNF(typename DataPoint::Scalar lowPrecisionEpsilon = typename DataPoint::Scalar(0.1)) // Lesser precision for the paraboloid test
-{
-    // Define related structure
-    typedef typename DataPoint::Scalar Scalar;
-    typedef typename DataPoint::VectorType VectorType;
-    typedef Basket<DataPoint, NF, OrientedSphereFit> Fit;
-    typedef Basket<DataPoint, NFG, OrientedSphereFit> FitG;
-
-    // generate samples
-    int nbPoints = Eigen::internal::random<int>(100, 1000);
-    int nbPointsFit = 50;
-
-    // equal probability of having a plane or a random quadric
-    VectorType coeff = 5 * VectorType::Random();
-    if(Eigen::internal::random<Scalar>(0., 1.) < Scalar(0.5))
-    {
-        coeff = VectorType::Zero();
-    }
-    // avoid saddles, which can cause issues with the sphere fitting
-    coeff.y() = std::copysign(coeff.y(), coeff.x());
-
-    Scalar width = Eigen::internal::random<Scalar>(1., 10.);
-    VectorType center = 1000 * VectorType::Random();
-
-    Scalar zmax = std::abs((coeff[0] + coeff[1]) * width*width);
-    Scalar analysisScale = std::sqrt(zmax*zmax + width*width);
-
-    Fit fit;
-    FitG fitG;
-    fit.setNeighborFilter(NF(center, analysisScale));
-    fitG.setNeighborFilter(NFG(center, analysisScale));
-    fit.init();
-    fitG.init();
-
-    for(int i = 0; i < nbPointsFit; ++i)
-    {
-        DataPoint p = getPointOnParaboloid<DataPoint>(coeff.x(),
-                                                      coeff.y(),
-                                                      width,
-                                                      false);           // noise
-        p.pos() += center;
-
-        fit.addNeighbor(p);
-        fitG.addNeighbor(DataPoint(p.pos(), p.normal()));
-    }
-    std::cout << "------------------------------------------------ IN fit.finalize() ------------------------------------------------ " << std::endl;
-    fit.finalize(__DBL_NORM_MAX__);
-    std::cout << "------------------------------------------------ IN fitG.finalize() ----------------------------------------------- " << std::endl;
-    fitG.finalize();
-    std::cout << "------------------------------------------------------------------------------------------------------------------- " << std::endl;
-
-    if(fit.isStable() && fitG.isStable())
-    {
-        std::vector<VectorType> samples (nbPoints);
-        for (int i = 0; i < nbPoints; ++i)
-        {
-            VectorType p = center + analysisScale * VectorType::Random();
-            samples[i] = p;
-
-            if (std::abs(fitG.potential(fitG.project(p))) >= lowPrecisionEpsilon)
-            {
-                std::cout << "------------------------------------------------ IN testFunction() ------------------------------------------------ " << std::endl;
-            	std::cout << std::left << std::setw(26);
-                std::cout << "fit.project(p) : " << fit.project(p).transpose();
-            	std::cout << std::right << std::setw(25);
-                std::cout << "| fitG.project(p) : " << fitG.project(p).transpose();
-                std::cout << "\n";
-
-            	std::cout << std::left << std::setw(25);
-                std::cout << "fit.potential(fit.project(p)) : " << fit.potential(fit.project(p));
-            	std::cout << std::right << std::setw(60);
-                std::cout << "| fitG.potential(fitG.project(p)) : " << fitG.potential(fitG.project(p));
-                std::cout << "\n" << endl;
-
-            }
-
-            // check that the projected point is on the surface
-            VERIFY( std::abs(fit.potential(fit.project(p))) < lowPrecisionEpsilon );
-            VERIFY( std::abs(fitG.potential(fitG.project(p))) < lowPrecisionEpsilon );
-        }
-
-        // check if direct projection gives same or better result than descent projection.
-        for( const auto& p: samples )
-        {
-            VectorType res1 = fit.project( p );
-            VectorType res2 = fit.projectDescent( p, 1000 ); // force high number of iterations
-            VERIFY( res1.isApprox( res2, lowPrecisionEpsilon ));
-            VectorType res1G = fitG.project( p );
-            VectorType res2G = fitG.projectDescent( p, 1000 ); // force high number of iterations
-            VERIFY( res1G.isApprox( res2G, lowPrecisionEpsilon ));
-        }
-    }
-}
-
 template<typename Scalar, int Dim>
 void callSubTests()
 {
@@ -227,8 +129,6 @@ void callSubTests()
     cout << "Testing with parabola..." << endl;
     for(int i = 0; i < g_repeat; ++i)
     {
-        // CALL_SUBTEST(( compareNF<Point, WeightSmoothFunc, WeightSmoothFuncGlobal>(0.001) ));
-        // CALL_SUBTEST(( compareNF<Point, WeightConstantFunc, WeightConstantFuncGlobal>(0.001) ));
         CALL_SUBTEST(( testFunction<Point, WeightSmoothFunc>() ));
         CALL_SUBTEST(( testFunction<Point, WeightSmoothFuncGlobal, false>(0.1) ));
         CALL_SUBTEST(( testFunction<Point, WeightConstantFunc>() ));
