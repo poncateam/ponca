9fcc7106a76c9524fa25078919d04968faa20339
fauberva <florian.auberval@irit.fr>
2025-09-11 15:53:10 +0200

    [fitting] Moving the computeMLS functions to the macro definition

--- Ponca/src/Fitting/basket.h
+++ Ponca/src/Fitting/basket.h
@@ -150,7 +150,59 @@ namespace internal
             res = this->finalize();                                                                   \
         } while ( res == NEED_OTHER_PASS );                                                           \
         return res;                                                                                   \
-    }
+    }                                                                                                 \
+        /*! \brief Computes the fit using the MLS iteration process. */                               \
+        /*! The position of the projected point is outputted through the lastPosition argument. */    \
+        /*!  @tparam PointContainer STL-like container storing the points */                          \
+        /*!  @param points An STL-like container of points */                                         \
+        /*!  @param mlsIter The amount of MLS iteration that is being done for this fit */            \
+        /*!  @return The result of the fit */                                                         \
+        template<typename PointContainer>                                                             \
+        FIT_RESULT computeMLS(const PointContainer& points, const int mlsIter=5) {                    \
+            assert(mlsIter > 0);                                                                      \
+            FIT_RESULT res     = UNDEFINED;                                                           \
+            const Scalar t     = Base::m_w.evalScale();                                               \
+            auto lastPos       = Base::m_w.evalPos();                                                 \
+            for( int mm = 0; mm < mlsIter; ++mm) {                                                    \
+                Base::setWeightFunc({lastPos, t});                                                    \
+                res = compute(points);                                                                \
+                if (Base::isStable()) {                                                               \
+                    lastPos = Base::project( lastPos );                                               \
+                } else {                                                                              \
+                    std::cerr << "Warning: fit at mls iteration " << mm << " is not stable" << std::endl;\
+                    break;                                                                            \
+                }                                                                                     \
+            }                                                                                         \
+            return res;                                                                               \
+        }                                                                                             \
+        /*! \brief Computes the fit using the MLS iteration process.                            */    \
+        /*! The position of the projected point is outputted through the lastPosition argument. */    \
+        /*!  @tparam IndexRange STL-Like range storing indices                                  */    \
+        /*!  @tparam PointContainer STL-like container storing the points                       */    \
+        /*!  @param ids An STL-like container of indeices of the neighbors                      */    \
+        /*!  @param points An STL-like container of points                                      */    \
+        /*!  @param mlsIter The amount of MLS iteration that is being done for this fit         */    \
+        /*!  @return The result of the fit                                                      */    \
+        template<typename IndexRange, typename PointContainer>                                        \
+        FIT_RESULT computeWithIdsMLS(                                                                 \
+            const IndexRange& ids, const PointContainer& points, const int mlsIter=5                  \
+        ) {                                                                                           \
+            assert(mlsIter > 0);                                                                      \
+            FIT_RESULT res     = UNDEFINED;                                                           \
+            const Scalar t     = Base::m_w.evalScale();                                               \
+            auto lastPos       = Base::m_w.evalPos();                                                 \
+            for( int mm = 0; mm < mlsIter; ++mm) {                                                    \
+                Base::setWeightFunc({lastPos, t});                                                    \
+                res = computeWithIds(ids, points);                                                    \
+                if (Base::isStable()) {                                                               \
+                    lastPos = Base::project( lastPos );                                               \
+                } else {                                                                              \
+                    std::cerr << "Warning: fit at mls iteration " << mm << " is not stable" << std::endl;\
+                    break;                                                                            \
+                }                                                                                     \
+            }                                                                                         \
+            return res;                                                                               \
+        }                                                                                             \
 
     /*!
          \brief Aggregator class used to declare specialized structures with derivatives computations, using CRTP
@@ -273,69 +325,6 @@ namespace internal
                 return true;
             }
             return false;
-        }
-
-        /*! \brief Computes the fit using the MLS iteration process.
-         * The position of the projected point is outputted through the lastPosition argument.
-         *
-         * @tparam PointContainer STL-like container storing the points
-         * @param points An STL-like container of points
-         * @param mlsIter The amount of MLS iteration that is being done for this fit
-         * @return The result of the fit
-         */
-        template<typename PointContainer>
-        FIT_RESULT computeMLS(const PointContainer& points, const int mlsIter=5) {
-            assert(mlsIter > 0);
-            FIT_RESULT res     = UNDEFINED;
-            const int t        = Base::m_w.evalScale();
-            VectorType lastPos = Base::m_w.evalPos();
-
-            // MLS Iteration
-            for( int mm = 0; mm < mlsIter; ++mm) {
-                Base::setWeightFunc({lastPos, t});
-                // Initialize the fit and starts a new pass
-                res = compute(points);
-
-                if (Base::isStable()) {
-                    lastPos = Base::project( lastPos ); // Update the position
-                } else {
-                    std::cerr << "Warning: fit at mls iteration " << mm << " is not stable" << std::endl;
-                    break;
-                }
-            }
-            return res;
-        }
-        /*! \brief Computes the fit using the MLS iteration process.
-         * The position of the projected point is outputted through the lastPosition argument.
-         *
-         * @tparam IndexRange STL-Like range storing indices
-         * @tparam PointContainer STL-like container storing the points
-         * @param ids An STL-like container of indeices of the neighbors
-         * @param points An STL-like container of points
-         * @param mlsIter The amount of MLS iteration that is being done for this fit
-         * @return The result of the fit
-         */
-        template<typename IndexRange, typename PointContainer>
-        FIT_RESULT computeWithIdsMLS(const IndexRange& ids, const PointContainer& points, const int mlsIter=5) {
-            assert(mlsIter > 0);
-            FIT_RESULT res     = UNDEFINED;
-            const int t        = Base::m_w.evalScale();
-            VectorType lastPos = Base::m_w.evalPos();
-
-            // MLS Iteration
-            for( int mm = 0; mm < mlsIter; ++mm) {
-                Base::setWeightFunc({lastPos, t});
-                // Initialize the fit and starts a new pass
-                res = computeWithIds(ids, points);
-
-                if (Base::isStable()) {
-                    lastPos = Base::project( lastPos ); // Update the position
-                } else {
-                    std::cerr << "Warning: fit at mls iteration " << mm << " is not stable" << std::endl;
-                    break;
-                }
-            }
-            return res;
         }
     }; // class Basket
 
