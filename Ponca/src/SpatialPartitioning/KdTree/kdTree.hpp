40b896bad6ce14448926ca594a7e56d12baba784
fauberva <florian.auberval@irit.fr>
2025-09-26 13:26:14 +0200

    [kdtree] Adding a static cast

--- Ponca/src/SpatialPartitioning/KdTree/kdTree.hpp
+++ Ponca/src/SpatialPartitioning/KdTree/kdTree.hpp
@@ -163,7 +163,7 @@ void KdTreeBase<Traits>::build_rec(NodeIndexType node_id, IndexType start, Index
     {
         IndexType split_dim = 0;
         (Scalar(0.5) * aabb.diagonal()).maxCoeff(&split_dim);
-        node.configure_inner(aabb.center()[split_dim], m_nodes.size(), split_dim);
+        node.configure_inner(aabb.center()[split_dim], static_cast<IndexType>(m_nodes.size()), split_dim);
         m_nodes.emplace_back();
         m_nodes.emplace_back();
 
