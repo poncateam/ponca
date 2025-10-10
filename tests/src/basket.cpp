6010db59c63a7538c0c72836e89d80efbc687e4c
Florian <57592360+Me-k-01@users.noreply.github.com>
2025-10-10 09:26:49 +0200

    Update tests/src/basket.cpp

--- tests/src/basket.cpp
+++ tests/src/basket.cpp
@@ -179,7 +179,6 @@ void callSubTests()
     //! [FitType]
     using Sphere = Basket<Point, NeighborFilter, OrientedSphereFit>;
     //! [FitType]
-
     //! [HybridType]
     // Create an hybrid structure fitting a plane and a sphere at the same time
     using Hybrid = Basket<Point, NeighborFilter,
