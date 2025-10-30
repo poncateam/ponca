fbb02c75d52d26325cbb946c7ef3578f9286fbe6
nmellado <nmellado@users.noreply.github.com>
2025-10-30 20:43:04 +0000

    deploy: b63e69866b9b277a96802d3d06e6492d50ffc055

--- classPonca_1_1Basket.js
+++ classPonca_1_1Basket.js
@@ -2,7 +2,7 @@ var classPonca_1_1Basket =
 [
     [ "Base", "classPonca_1_1Basket.html#a00aba788f149092e3032e109d4ed0c52", null ],
     [ "DataPoint", "classPonca_1_1Basket.html#a4e7ea1fd0f8d760217e60c1feaabf7b0", null ],
-    [ "NeighborhoodFilter", "classPonca_1_1Basket.html#ac2383b4705dfdfd08c08c0d0d7567bbd", null ],
+    [ "NeighborFilter", "classPonca_1_1Basket.html#ab536eeefa247e16a1de59ab3fe60bfa5", null ],
     [ "Scalar", "classPonca_1_1Basket.html#ab08e85c1c9d83a892d6dc3beeb593e2d", null ],
     [ "addNeighbor", "classPonca_1_1Basket.html#ad5132ec839c56fdd21e875825d21465a", null ],
     [ "projectDescent", "classPonca_1_1Basket.html#a983039cc23df1537214de05541697d33", null ]
--- classPonca_1_1BasketDiff.js
+++ classPonca_1_1BasketDiff.js
@@ -1,7 +1,7 @@
 var classPonca_1_1BasketDiff =
 [
     [ "DataPoint", "classPonca_1_1BasketDiff.html#a24cafc02fc3353e9e0a165250de23609", null ],
+    [ "NeighborFilter", "classPonca_1_1BasketDiff.html#a76dbf7d6bb9b0948be0a3698a2a6e7a5", null ],
     [ "Scalar", "classPonca_1_1BasketDiff.html#ad71dbb99d45a869c675cb487f7857a53", null ],
-    [ "WeightFunction", "classPonca_1_1BasketDiff.html#a4567bef02f3307172494467419d243ba", null ],
     [ "addNeighbor", "classPonca_1_1BasketDiff.html#a5ea7daddae9d7bd58b3da8fc99ff31b4", null ]
 ];
