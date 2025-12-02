jmaleo/ponca_mean_plane

PlaneFrame => LocalFrame

tests/src/fit_plane.cpp
@@ -145,11 +145,11 @@ void callSubTests()
 
     // test if conflicts are detected
     //! [Conflicting type]
-    typedef Basket<Point, WeightConstantFunc, Plane, PlaneFrame,
+    typedef Basket<Point, WeightConstantFunc, Plane, LocalFrame,
             MeanNormal, MeanPosition, MeanPlaneFitImpl,
             CovarianceFitBase, CovariancePlaneFitImpl> Hybrid1; //test conflict detection in one direction
     //! [Conflicting type]
-    typedef Basket<Point, WeightConstantFunc, Plane, PlaneFrame,
+    typedef Basket<Point, WeightConstantFunc, Plane, LocalFrame,
             MeanPosition, CovarianceFitBase, CovariancePlaneFitImpl,
             MeanNormal, MeanPlaneFitImpl> Hybrid2;  //test conflict detection in the second direction
 

poncateam/ponca

tests/src/fit_plane.cpp
@@ -155,11 +155,11 @@ void callSubTests()
 
     // test if conflicts are detected
     //! [Conflicting type]
-    typedef Basket<Point, NoWeightFuncGlobal, Plane, PlaneFrame,
+    typedef Basket<Point, NoWeightFuncGlobal, Plane, LocalFrame,
             MeanNormal, MeanPosition, MeanPlaneFitImpl,
             CovarianceFitBase, CovariancePlaneFitImpl> Hybrid1; //test conflict detection in one direction
     //! [Conflicting type]
-    typedef Basket<Point, NoWeightFuncGlobal, Plane, PlaneFrame,
+    typedef Basket<Point, NoWeightFuncGlobal, Plane, LocalFrame,
             MeanPosition, CovarianceFitBase, CovariancePlaneFitImpl,
             MeanNormal, MeanPlaneFitImpl> Hybrid2;  //test conflict detection in the second direction
 