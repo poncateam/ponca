d0515de88f2300f1f2403629d9efe3b2feac8849
Nicolas Mellado <nmellado0@gmail.com>
2025-12-03 17:30:21 -0500

    [tests] Attempt to include all files for better coverage report.

--- tests/common/testing.h
+++ tests/common/testing.h
@@ -39,6 +39,12 @@ static bool QUICK_TESTS =
 false;
 #endif
 
+#ifdef PONCA_COVERAGE_ENABLED
+#include "Ponca/Common"
+#include "Ponca/Fitting"
+#include "Ponca/SpatialPartitioning"
+#endif
+
 #define PONCA_PP_MAKE_STRING2(S) #S
 #define PONCA_PP_MAKE_STRING(S) PONCA_PP_MAKE_STRING2(S)
 
