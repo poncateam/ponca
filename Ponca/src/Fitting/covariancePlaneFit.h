jmaleo/ponca_mean_plane

Update classes using PlaneFrame

Ponca/src/Fitting/covariancePlaneFit.h
@@ -13,6 +13,7 @@
 #include "./primitive.h"
 #include "./mean.h"          // used to define CovarianceLineFit
 #include "./covarianceFit.h" // use to define CovariancePlaneFit
+#include "./planeFrame.h"    // use to define CovariancePlaneFit
 
 #include <Eigen/Eigenvalues>
 
@@ -38,13 +39,12 @@ PONCA_FITTING_DECLARE_MATRIX_TYPE
 protected:
     enum
     {
-        Check = Base::PROVIDES_PLANE &&
-                Base::PROVIDES_POSITION_COVARIANCE,
+        Check = Base::PROVIDES_POSITION_COVARIANCE 
+            and Base::PROVIDES_PLANE_FRAME,
         /*!
-         * \brief Expose a method worldToTangentPlane(VectorType), which turns a point
+         * \brief Fit the tangent plane and store it into Plane and PlaneFrame which turn a point
          * in ambient 3D space to the tangent plane.
-         * \see worldToTangentPlane
-         * \see tangentPlaneToWorld
+         * \see PlaneFrame
          */
         PROVIDES_TANGENT_PLANE_BASIS
     };
@@ -53,31 +53,6 @@ public:
     PONCA_EXPLICIT_CAST_OPERATORS(CovariancePlaneFitImpl,covariancePlaneFit)
     PONCA_FITTING_DECLARE_FINALIZE
 
-    /**************************************************************************/
-    /* Results                                                                */
-    /**************************************************************************/
-
-    /*!
-     * \brief Express a point in ambient space relatively to the tangent plane.
-     *
-     * Output vector is: [h, u, v]^T, where u, v are 2d coordinates on the plane,
-     * and h the height of the sample.
-     * \tparam ignoreTranslation must be set to true when passing vectors instead of points
-     * \param _q Point coordinates expressed in ambient space
-     * \return Point coordinates expressed in local tangent frame
-     */
-    template <bool ignoreTranslation = false>
-    PONCA_MULTIARCH inline VectorType worldToTangentPlane(const VectorType &_q) const;
-
-    /*!
-     * \brief Transform a point from the tangent plane [h, u, v]^T to ambient space
-     *
-     * \tparam ignoreTranslation must be set to true when passing vectors instead of points
-     * \param _q Point coordinates expressed in local tangent frame
-     * \return Point coordinates expressed in ambient space
-     */
-    template <bool ignoreTranslation = false>
-    PONCA_MULTIARCH inline VectorType tangentPlaneToWorld(const VectorType &_q) const;
 }; //class CovariancePlaneFitImpl
 
 /// \brief Helper alias for Plane fitting on 3D points using CovariancePlaneFitImpl
@@ -87,7 +62,8 @@ public:
     CovariancePlaneFitImpl<DataPoint, _WFunctor,
             CovarianceFitBase<DataPoint, _WFunctor,
                     MeanPosition<DataPoint, _WFunctor,
-                            Plane<DataPoint, _WFunctor,T>>>>;
+                        PlaneFrame<DataPoint, _WFunctor,
+                            Plane<DataPoint, _WFunctor,T>>>>>;
 //! [CovariancePlaneFit Definition]
 
 /*!

tests/src/fit_plane.cpp
@@ -145,11 +145,11 @@ void callSubTests()
 
     // test if conflicts are detected
     //! [Conflicting type]
-    typedef Basket<Point, WeightConstantFunc, Plane,
+    typedef Basket<Point, WeightConstantFunc, Plane, PlaneFrame,
             MeanNormal, MeanPosition, MeanPlaneFitImpl,
             CovarianceFitBase, CovariancePlaneFitImpl> Hybrid1; //test conflict detection in one direction
     //! [Conflicting type]
-    typedef Basket<Point, WeightConstantFunc, Plane,
+    typedef Basket<Point, WeightConstantFunc, Plane, PlaneFrame,
             MeanPosition, CovarianceFitBase, CovariancePlaneFitImpl,
             MeanNormal, MeanPlaneFitImpl> Hybrid2;  //test conflict detection in the second direction
 

poncateam/ponca

Ponca/src/Fitting/covariancePlaneFit.h
@@ -14,6 +14,7 @@
 #include "./primitive.h"
 #include "./mean.h"          // used to define CovarianceLineFit
 #include "./covarianceFit.h" // use to define CovariancePlaneFit
+#include "./planeFrame.h"    // use to define CovariancePlaneFit
 
 #include <Eigen/Eigenvalues>
 
@@ -39,13 +40,12 @@ PONCA_FITTING_DECLARE_MATRIX_TYPE
 protected:
     enum
     {
-        Check = Base::PROVIDES_PLANE &&
-                Base::PROVIDES_POSITION_COVARIANCE,
+        Check = Base::PROVIDES_POSITION_COVARIANCE
+            and Base::PROVIDES_PLANE_FRAME,
         /*!
-         * \brief Expose a method worldToTangentPlane(VectorType), which turns a point
+         * \brief Fit the tangent plane and store it into Plane and PlaneFrame which turn a point
          * in ambient 3D space to the tangent plane.
-         * \see worldToTangentPlane
-         * \see tangentPlaneToWorld
+         * \see PlaneFrame
          */
         PROVIDES_TANGENT_PLANE_BASIS
     };
@@ -55,27 +55,6 @@ public:
     PONCA_FITTING_DECLARE_FINALIZE
     PONCA_FITTING_IS_SIGNED(false)
 
-    /**************************************************************************/
-    /* Results                                                                */
-    /**************************************************************************/
-
-    /*!
-     * \brief Express a point in ambient space relatively to the tangent plane.
-     *
-     * Output vector is: [h, u, v]^T, where u, v are 2d coordinates on the plane,
-     * and h the height of the sample.
-     * \param _q Point coordinates expressed in ambient space
-     * \return Point coordinates expressed in local tangent frame
-     */
-    PONCA_MULTIARCH inline VectorType worldToTangentPlane(const VectorType &_q) const;
-
-    /*!
-     * \brief Transform a point from the tangent plane [h, u, v]^T to ambient space
-     *
-     * \param _q Point coordinates expressed in local tangent frame
-     * \return Point coordinates expressed in ambient space
-     */
-    PONCA_MULTIARCH inline VectorType tangentPlaneToWorld(const VectorType &_q) const;
 }; //class CovariancePlaneFitImpl
 
 /// \brief Helper alias for Plane fitting on 3D points using CovariancePlaneFitImpl
@@ -85,7 +64,8 @@ public:
     CovariancePlaneFitImpl<DataPoint, _NFilter,
             CovarianceFitBase<DataPoint, _NFilter,
                     MeanPosition<DataPoint, _NFilter,
-                            Plane<DataPoint, _NFilter, T>>>>;
+                        PlaneFrame<DataPoint, _NFilter,
+                            Plane<DataPoint, _NFilter,T>>>>>;
 //! [CovariancePlaneFit Definition]
 
 /*!

tests/src/fit_plane.cpp
@@ -158,11 +158,11 @@ void callSubTests()
 
     // test if conflicts are detected
     //! [Conflicting type]
-    typedef Basket<Point, NoWeightFuncGlobal, Plane,
+    typedef Basket<Point, NoWeightFuncGlobal, Plane, PlaneFrame,
             MeanNormal, MeanPosition, MeanPlaneFitImpl,
             CovarianceFitBase, CovariancePlaneFitImpl> Hybrid1; //test conflict detection in one direction
     //! [Conflicting type]
-    typedef Basket<Point, NoWeightFuncGlobal, Plane,
+    typedef Basket<Point, NoWeightFuncGlobal, Plane, PlaneFrame,
             MeanPosition, CovarianceFitBase, CovariancePlaneFitImpl,
             MeanNormal, MeanPlaneFitImpl> Hybrid2;  //test conflict detection in the second direction
 