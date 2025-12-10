6ec35f73f5aa16eae9973efd662a77bdcbcfae5f
Nicolas Mellado <nmellado0@gmail.com>
2025-12-10 07:06:47 -0500

    [Fitting] Add NormalDerivativeWeingartenEstimator
    Fixme: need to add tests and remove duplicated code in NormalDerivativesCurvatureEstimator

--- Ponca/src/Fitting/curvature.hpp
+++ Ponca/src/Fitting/curvature.hpp
@@ -96,6 +96,73 @@ namespace Ponca {
         return (L*N-M*M)/(E*G-F*F);
     }
 
+    ///////// NormalDerivativeWeingartenEstimator
+    template<class DataPoint, class _NFilter, int DiffType, typename T>
+    typename NormalDerivativeWeingartenEstimator<DataPoint, _NFilter, DiffType, T>::Matrix2
+    NormalDerivativeWeingartenEstimator<DataPoint, _NFilter, DiffType, T>::weingartenMap() const {
+        Matrix2 w;
+        weingartenMap(w);
+        return w;
+    }
+
+    template<class DataPoint, class _NFilter, int DiffType, typename T>
+    template<typename Matrix2Derived>
+    void NormalDerivativeWeingartenEstimator<DataPoint, _NFilter, DiffType, T>::weingartenMap(Matrix2Derived &W) const {
+        PONCA_MULTIARCH_STD_MATH(abs);
+        PONCA_MULTIARCH_STD_MATH(sqrt);
+
+        using Index = typename VectorType::Index;
+        using Matrix32 = Eigen::Matrix<Scalar,3,2>;
+
+        // Get the object space Weingarten map dN
+        MatrixType dN = Base::dNormal().template middleCols<DataPoint::Dim>(Base::isScaleDer() ? 1: 0);
+
+        // Compute tangent-space basis
+        // \FIXME This code provides tangent plane estimation, but does not make it available to other classes
+        //        Could be interesting to make it available through PROVIDES_TANGENT_PLANE_BASIS
+        Matrix32 B;
+        {
+            Index i0=Index(-1), i1=Index(-1), i2=Index(-1);
+            // Use the spatial derivative of the normal. This option leads to NaN
+            // values if dN is null (like in the case of a perfect plane)
+
+            // Get the object space Weingarten map dN
+            MatrixType dN = Base::dNormal().template middleCols<DataPoint::Dim>(Base::isScaleDer() ? 1 : 0);
+
+            // Compute tangent-space basis from dN
+            //   1 - pick the column with maximal norm as the first tangent vector,
+            Scalar sqNorm = dN.colwise().squaredNorm().maxCoeff(&i0);
+            B.col(0) = dN.col(i0) / sqrt(sqNorm);
+            //   2 - orthogonalize the other column vectors, and pick the most reliable one
+            i1 = (i0 + 1) % 3;
+            i2 = (i0 + 2) % 3;
+            VectorType v1 = dN.col(i1) - B.col(0).dot(dN.col(i1)) * B.col(0);
+            VectorType v2 = dN.col(i2) - B.col(0).dot(dN.col(i2)) * B.col(0);
+            Scalar v1norm2 = v1.squaredNorm();
+            Scalar v2norm2 = v2.squaredNorm();
+            if (v1norm2 > v2norm2) B.col(1) = v1 / sqrt(v1norm2);
+            else B.col(1) = v2 / sqrt(v2norm2);
+        }
+
+        // Compute the 2x2 matrix representing the shape operator by transforming dN to the basis B.
+        // Recall that dN is a bilinear form, it thus transforms as follows:
+        W = B.transpose() * dN * B;
+
+        // Recall that at this stage, the shape operator represented by W describes the normal curvature K_n(u) in the direction u \in R^2 as follows:
+        //   K_n(u) = u^T W u
+        // The principal curvatures are fully defined by the values and the directions of the extrema of K_n.
+        //
+        // If the normal field N(x) comes from the gradient of a scalar field, then N(x) is curl-free, and dN and W are symmetric matrices.
+        // In this case, the extrema of the previous quadratic form are directly obtained by the eigenvalue decomposition of W.
+        // However, if N(x) is only an approximation of the normal field of a surface, then N(x) is not necessarily curl-free, and in this case W is not symmetric.
+        // In this case, we first have to find an equivalent symmetric matrix W' such that:
+        //   K_n(u) = u^T W' u,
+        // for any u \in R^2.
+        // It is easy to see that such a W' is simply obtained as:
+        //   W' = (W + W^T)/2
+        W(0,1) = W(1,0) = (W(0,1) + W(1,0))/Scalar(2);
+    }
+
     ///////// WeingartenCurvatureEstimator
     template<class DataPoint, class _NFilter, typename T>
     typename WeingartenCurvatureEstimatorBase<DataPoint, _NFilter, T>::Scalar
