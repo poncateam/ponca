5d7dfc96c0a7bf65e38ec09fd78876d4e924fcf8
fauberva <auberval.florian01@gmail.com>
2025-12-12 16:16:14 +0100

    [ci] Mock-up for CUDA example on 3D point cloud
    Signed-off-by: fauberva <auberval.florian01@gmail.com>

--- examples/cuda-point-cloud/ponca_point_cloud.cu
+++ examples/cuda-point-cloud/ponca_point_cloud.cu
@@ -9,49 +9,69 @@
 #include <Ponca/src/SpatialPartitioning/KdTree/kdTree.h>
 #include <vector>
 
-//
-// template<typename DataPoint>
-// typename DataPoint::Scalar generateData(Ponca::KdTree<DataPoint>& tree)
-// {
-//     typedef typename DataPoint::Scalar Scalar;
-//     typedef typename DataPoint::VectorType VectorType;
-//
-//     //generate sampled sphere
-// #ifdef NDEBUG
-//     int nbPoints = Eigen::internal::random<int>(500, 1000);
-// #else
-//     int nbPoints = Eigen::internal::random<int>(100, 200);
-// #endif
-//
-//     Scalar radius = Eigen::internal::random<Scalar>(1., 10.);
-//
-//     Scalar analysisScale = Scalar(10.) * std::sqrt( Scalar(4. * M_PI) * radius * radius / nbPoints);
-//     Scalar centerScale = Eigen::internal::random<Scalar>(1,10000);
-//     VectorType center = VectorType::Random() * centerScale;
-//
-//     std::vector<DataPoint> vectorPoints(nbPoints);
-//
-// #ifdef NDEBUG
-// #pragma omp parallel for
-// #endif
-//     for(int i = 0; i < int(vectorPoints.size()); ++i)
-//     {
-//         vectorPoints[i] = getPointOnSphere<DataPoint>(radius, center, false, false, false);
-//     }
-//
-//     tree.clear();
-//     tree.build(vectorPoints);
-//
-//     return analysisScale;
-// }
+#include "../common/testing.h"
+#include "../common/testUtils.h"
 
-template<typename Fit>
-__global__ void kernel()
+template<typename Fit, typename Tree, typename Scalar>
+__global__ void fitKernel(const Tree& tree, const Scalar analysisScale)
 {
-    // Fit fit;
-    // fit.setWeightFunc({ });
-    // fit.init();
+    std::vector<DataPoint> vectorPoints = tree.points();
+    // Global index
+    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    if (i >= vectorPoints.size())
+        return;
+
+    Fit fit;
+    fit.setWeightFunc({ vectorPoints[i].pos(), analysisScale });
+    fit.init();
+
+    // TODO : fit compute and exit values
 }
 
-int main()
-{}
+template<typename Scalar, typename Dim>
+__host__ int testPlaneCuda(bool _bUnoriented = false, bool _bAddPositionNoise = false, bool _bAddNormalNoise = false, bool conflictAnnounced = false)
+{
+    typedef PointPositionNormal<Scalar, Dim> DataPoint;
+    typedef DistWeightFunc<Point, SmoothWeightKernel<Scalar> > WeightSmoothFunc;
+    typedef Basket<Point, WeightSmoothFunc, MeanPlaneFit> MeanFitSmooth;
+    typedef typename DataPoint::VectorType VectorType;
+
+    int nbPoints = Eigen::internal::random<int>(100, 1000);
+
+    Scalar width  = Eigen::internal::random<Scalar>(1., 10.);
+    Scalar height = width;
+
+    Scalar analysisScale = Scalar(15.) * std::sqrt( width * height / nbPoints);
+    Scalar centerScale   = Eigen::internal::random<Scalar>(1,10000);
+    VectorType center    = VectorType::Random() * centerScale;
+
+    VectorType direction = VectorType::Random().normalized();
+
+    Scalar epsilon = testEpsilon<Scalar>();
+    std::vector<DataPoint> vectorPoints(nbPoints);
+
+    for(unsigned int i = 0; i < vectorPoints.size(); ++i)
+    {
+        vectorPoints[i] = getPointOnPlane<DataPoint>(center,
+                                                     direction,
+                                                     width,
+                                                     _bAddPositionNoise,
+                                                     _bAddNormalNoise,
+                                                     _bUnoriented);
+    }
+
+    KdTreeDense<DataPoint> tree(vectorPoints);
+
+    std::cout << "Plane fit" << std::endl;
+    fitKernel<MeanFitSmooth><<<1, 256>>>(tree, analysisScale);
+    cudaDeviceSynchronize();
+}
+
+
+__host__ int main(int argc, char** argv) {
+    if(!init_testing(argc, argv))
+        return EXIT_FAILURE;
+
+    std::cout << "Test plane fitting on cuda..." << std::endl;
+    testPlaneCuda<float, 3>();
+}
