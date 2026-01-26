jmaleo/ponca_mean_plane

PlaneFrame => LocalFrame

Ponca/src/Fitting/covariancePlaneFit.h
@@ -13,7 +13,7 @@
 #include "./primitive.h"
 #include "./mean.h"          // used to define CovarianceLineFit
 #include "./covarianceFit.h" // use to define CovariancePlaneFit
-#include "./planeFrame.h"    // use to define CovariancePlaneFit
+#include "./localFrame.h"    // use to define CovariancePlaneFit
 
 #include <Eigen/Eigenvalues>
 
@@ -39,7 +39,7 @@ PONCA_FITTING_DECLARE_MATRIX_TYPE
 protected:
     enum
     {
-        Check = Base::PROVIDES_POSITION_COVARIANCE && Base::PROVIDES_PLANE_FRAME,
+        Check = Base::PROVIDES_POSITION_COVARIANCE && Base::PROVIDES_LOCAL_FRAME,
         /*!
          * \brief Fit the tangent plane and store it into Plane and PlaneFrame which turn a point
          * in ambient 3D space to the tangent plane.
@@ -61,7 +61,7 @@ public:
     CovariancePlaneFitImpl<DataPoint, _WFunctor,
             CovarianceFitBase<DataPoint, _WFunctor,
                     MeanPosition<DataPoint, _WFunctor,
-                        PlaneFrame<DataPoint, _WFunctor,
+                        LocalFrame<DataPoint, _WFunctor,
                             Plane<DataPoint, _WFunctor,T>>>>>;
 //! [CovariancePlaneFit Definition]
 

Ponca/src/Fitting/meanPlaneFit.h
@@ -10,7 +10,7 @@
 
 #include "./plane.h"
 #include "./mean.h"
-#include "./planeFrame.h"
+#include "./localFrame.h"
 
 namespace Ponca
 {
@@ -30,7 +30,7 @@ PONCA_FITTING_DECLARE_DEFAULT_TYPES
 PONCA_FITTING_DECLARE_MATRIX_TYPE
 
 protected:
-    enum { Check = Base::PROVIDES_PLANE_FRAME && Base::PROVIDES_PLANE };
+    enum { Check = Base::PROVIDES_LOCAL_FRAME && Base::PROVIDES_PLANE };
 
 
 public:
@@ -70,7 +70,7 @@ public:
     MeanPlaneFitImpl<DataPoint, _WFunctor,
         MeanNormal<DataPoint, _WFunctor,
             MeanPosition<DataPoint, _WFunctor,
-                PlaneFrame<DataPoint, _WFunctor,
+                LocalFrame<DataPoint, _WFunctor,
                     Plane<DataPoint, _WFunctor,T>>>>>;
 //! [MeanPlaneFit Definition]
 

tests/src/fit_plane.cpp
@@ -145,11 +145,11 @@ void callSubTests()
 
     // test if conflicts are detected
     //! [Conflicting type]
-    typedef Basket<Point, WeightConstantFunc, Plane, PlaneFrame,
+    typedef Basket<Point, WeightConstantFunc, Plane, LocalFrame,
             MeanNormal, MeanPosition, MeanPlaneFitImpl,
             CovarianceFitBase, CovariancePlaneFitImpl> Hybrid1; //test conflict detection in one direction
     //! [Conflicting type]
-    typedef Basket<Point, WeightConstantFunc, Plane, PlaneFrame,
+    typedef Basket<Point, WeightConstantFunc, Plane, LocalFrame,
             MeanPosition, CovarianceFitBase, CovariancePlaneFitImpl,
             MeanNormal, MeanPlaneFitImpl> Hybrid2;  //test conflict detection in the second direction
 

poncateam/ponca

Ponca/src/Fitting/covariancePlaneFit.h
@@ -14,7 +14,7 @@
 #include "./primitive.h"
 #include "./mean.h"          // used to define CovarianceLineFit
 #include "./covarianceFit.h" // use to define CovariancePlaneFit
-#include "./planeFrame.h"    // use to define CovariancePlaneFit
+#include "./localFrame.h"    // use to define CovariancePlaneFit
 
 #include <Eigen/Eigenvalues>
 
@@ -40,7 +40,7 @@ PONCA_FITTING_DECLARE_MATRIX_TYPE
 protected:
     enum
     {
-        Check = Base::PROVIDES_POSITION_COVARIANCE && Base::PROVIDES_PLANE_FRAME,
+        Check = Base::PROVIDES_POSITION_COVARIANCE && Base::PROVIDES_LOCAL_FRAME,
         /*!
          * \brief Fit the tangent plane and store it into Plane and PlaneFrame which turn a point
          * in ambient 3D space to the tangent plane.
@@ -63,7 +63,7 @@ public:
     CovariancePlaneFitImpl<DataPoint, _NFilter,
             CovarianceFitBase<DataPoint, _NFilter,
                     MeanPosition<DataPoint, _NFilter,
-                        PlaneFrame<DataPoint, _NFilter,
+                        LocalFrame<DataPoint, _NFilter,
                             Plane<DataPoint, _NFilter,T>>>>>;
 //! [CovariancePlaneFit Definition]
 

Ponca/src/Fitting/meanPlaneFit.h
@@ -10,7 +10,7 @@
 
 #include "./plane.h"
 #include "./mean.h"
-#include "./planeFrame.h"
+#include "./localFrame.h"
 
 namespace Ponca
 {
@@ -30,7 +30,7 @@ PONCA_FITTING_DECLARE_DEFAULT_TYPES
 PONCA_FITTING_DECLARE_MATRIX_TYPE
 
 protected:
-    enum { Check = Base::PROVIDES_PLANE_FRAME && Base::PROVIDES_PLANE };
+    enum { Check = Base::PROVIDES_LOCAL_FRAME && Base::PROVIDES_PLANE };
 
 
 public:
@@ -69,8 +69,8 @@ public:
     using MeanPlaneFit =
     MeanPlaneFitImpl<DataPoint, _NFilter,
         MeanNormal<DataPoint, _NFilter,
-            MeanNormal<DataPoint, _NFilter,
-                MeanPosition<DataPoint, _NFilter,
+            MeanPosition<DataPoint, _NFilter,
+                LocalFrame<DataPoint, _NFilter,
                     Plane<DataPoint, _NFilter, T>>>>>;
 //! [MeanPlaneFit Definition]
 

tests/src/fit_plane.cpp
@@ -158,11 +158,11 @@ void callSubTests()
 
     // test if conflicts are detected
     //! [Conflicting type]
-    typedef Basket<Point, NoWeightFuncGlobal, Plane, PlaneFrame,
+    typedef Basket<Point, NoWeightFuncGlobal, Plane, LocalFrame,
             MeanNormal, MeanPosition, MeanPlaneFitImpl,
             CovarianceFitBase, CovariancePlaneFitImpl> Hybrid1; //test conflict detection in one direction
     //! [Conflicting type]
-    typedef Basket<Point, NoWeightFuncGlobal, Plane, PlaneFrame,
+    typedef Basket<Point, NoWeightFuncGlobal, Plane, LocalFrame,
             MeanPosition, CovarianceFitBase, CovariancePlaneFitImpl,
             MeanNormal, MeanPlaneFitImpl> Hybrid2;  //test conflict detection in the second direction
 