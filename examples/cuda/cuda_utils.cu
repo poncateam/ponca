c179a5f659a87244138873671752f8df815f108f
fauberva <florian.auberval@irit.fr>
2026-01-28 11:16:54 +0100

    [examples] Simplify code and detect when CUDA instruction fails
    - Use `add_definitions` to run Eigen with CUDA
    - Use `add_definitions` to remove warnings triggered by Eigen declaring constexpr in __host__ function
    - Simplify point memory transfer by removing the interlaced array and the use of `PointPositionNormalLateBinding`
    - Add macro that detects when CUDA instruction silently fails
    - Remove util function that was equivalent to `Eigen::Map`
    - Free memory for points buffer
    - Use compute method in the CUDA example
    - Beautify the code
    
    Signed-off-by: fauberva <florian.auberval@irit.fr>

--- examples/cuda/cuda_utils.cu
+++ examples/cuda/cuda_utils.cu
@@ -1,48 +1,8 @@
 #pragma once
 
-/*!
- * \brief Extract a vector from a flattened array of vectors
- *
- * \tparam DataPoint The DataPoint type containing the VectorType and the number of dimensions.
- * \param idx The id of the vector that needs to be extracted (the index in the flattened array corresponds to : idx * number of dimensions).
- * \param flattenedVectorArray The flattened vector array, that is of size : total number of vector * Dimension.
- * \return The vector that was extracted from the flattened vector array.
- */
-template <typename DataPoint>
-PONCA_MULTIARCH typename DataPoint::VectorType extractVectorFromFlattenedArray(
-    const int idx,
-    const typename DataPoint::Scalar * const flattenedVectorArray
-) {
-    using VectorType = typename DataPoint::VectorType;
-    const int singleDimIndex = idx * DataPoint::Dim;
-    VectorType v;
-
-    for (int d = 0; d < DataPoint::Dim; ++d) {
-        v.row(d) << flattenedVectorArray[singleDimIndex + d];
-    }
-
-    return v;
-}
-
-/*! \brief Kernel that binds each point buffer to the interlaced buffer of positions and normals in the Device.
- *
- * \tparam DataPoint The DataPoint type.
- * \tparam Scalar A scalar type (float, double...)
- * \param points The point cloud (needs to be bind to the interlacedArray on the device).
- * \param interlacedArray The interlaced buffer of positions and normals.
- * \param nbPoints The total number of points in the point cloud.
- */
-template<typename DataPoint, typename Scalar>
-__global__ void bindPointsKernel(
-    DataPoint* points,
-    Scalar* interlacedArray,
-    const int nbPoints
-) {
-    // Get global index
-    const unsigned int i = threadIdx.x + blockIdx.x * blockDim.x;
-
-    // Skip when not in the point cloud
-    if (i >= nbPoints) return;
-
-    points[i].bind(interlacedArray);
+#define CUDA_CHECK(err) \
+    if (err != cudaSuccess) { \
+    std::cerr << "CUDA error: " << cudaGetErrorString(err) \
+    << " at " << __FILE__ << ":" << __LINE__ << std::endl; \
+    abort(); \
 }
--- examples/cuda/ponca_fit_plane.cu
+++ examples/cuda/ponca_fit_plane.cu
@@ -22,7 +22,7 @@
 template<typename DataPoint, typename Fit, typename Scalar>
 __global__ void fitPotentialAndGradientKernel(
     DataPoint* points,
-    const int nbPoints,
+    const unsigned int nbPoints,
     const Scalar analysisScale,
     Scalar* const potentialResults,
     Scalar* const gradientResults
@@ -35,18 +35,14 @@ __global__ void fitPotentialAndGradientKernel(
     // Skip when not in the point cloud
     if (i >= nbPoints) return;
 
-    auto pos = points[i].pos();
+    VectorType pos = points[i].pos();
 
     // Set up the fit
     Fit fit;
     fit.setNeighborFilter({ pos, analysisScale });
 
     // Computes the fit
-    fit.init();
-    for (int j = 0; j < nbPoints; ++j) {
-        fit.addNeighbor( points[j] );
-    }
-    fit.finalize();
+    fit.compute(points, points+nbPoints);
 
     // Returns NaN if not stable
     if (! fit.isStable()) {
@@ -58,8 +54,8 @@ __global__ void fitPotentialAndGradientKernel(
     }
 
     // Return the fit.potential result as an output
-    potentialResults[i] = fit.potential(pos);
-    VectorType grad = fit.primitiveGradient(pos);
+    potentialResults[i]   = fit.potential(pos);
+    const VectorType grad = fit.primitiveGradient(pos);
     for (int d = 0; d < DataPoint::Dim; ++d) {
         gradientResults[i*DataPoint::Dim + d] = grad(d);
     }
@@ -79,105 +75,84 @@ __host__ void testPlaneCuda(
     const bool _bAddPositionNoise = false,
     const bool _bAddNormalNoise   = false
 ) {
-    typedef Ponca::PointPositionNormalLateBinding<Scalar, Dim> DataPoint;
+    typedef Ponca::PointPositionNormal<Scalar, Dim> DataPoint;
     typedef Ponca::DistWeightFunc<DataPoint, Ponca::SmoothWeightKernel<Scalar> > WeightSmoothFunc;
     typedef Ponca::Basket<DataPoint, WeightSmoothFunc, Ponca::MeanPlaneFit> MeanFitSmooth;
     typedef typename DataPoint::VectorType VectorType;
 
     // Point cloud parameters for the plane
-    const int nbPoints = Eigen::internal::random<int>(100, 1000);
-    const Scalar width  = Eigen::internal::random<Scalar>(1., 10.);
-    const Scalar height = width;
-    const Scalar analysisScale = Scalar(15.) * std::sqrt( width * height / nbPoints);
-    const Scalar centerScale   = Eigen::internal::random<Scalar>(1, 10000);
-    const VectorType center    = VectorType::Random() * centerScale;
-    const VectorType direction = VectorType::Random().normalized();
+    const unsigned int nbPoints = Eigen::internal::random<int>(100, 1000);
+    const Scalar width          = Eigen::internal::random<Scalar>(1., 10.);
+    const Scalar height         = width;
+    const Scalar analysisScale  = Scalar(15.) * std::sqrt( width * height / nbPoints);
+    const Scalar centerScale    = Eigen::internal::random<Scalar>(1, 10000);
+    const VectorType center     = VectorType::Random() * centerScale;
+    const VectorType direction  = VectorType::Random().normalized();
 
     // The size of the data we send between Host and Device
     const unsigned long pointBufferSize      = nbPoints * sizeof(DataPoint);
     const unsigned long scalarBufferSize     = nbPoints * sizeof(Scalar);
     const unsigned long vectorBufferSize     = scalarBufferSize * Dim;
-    const unsigned long interlacedBufferSize = vectorBufferSize * 2;
 
-    // Interlaced array of position and normal values
-    auto* interlacedArray = new Scalar[nbPoints * Dim * 2];
-
+    // Generate the point cloud
+    std::vector<DataPoint> points(nbPoints);
     for(unsigned int i = 0; i < nbPoints; ++i) {
-        auto point = Ponca::getPointOnPlane<Ponca::PointPositionNormal<Scalar, Dim>>(
+        points[i] = Ponca::getPointOnPlane<DataPoint>(
             center, direction, width,
             _bAddPositionNoise, _bAddNormalNoise, _bUnoriented
         );
-        // Fill the interlaced array with the positions and normals.
-        for (int d = 0; d<Dim; ++d) {
-            interlacedArray[i * Dim * 2 + d]       = point.pos()[d];
-            interlacedArray[i * Dim * 2 + d + Dim] = point.normal()[d];
-        }
-    }
-
-    // Make a point buffer linking to the interlaced array
-    std::vector<DataPoint> points;
-    points.reserve(nbPoints);
-    for (int i = 0; i < nbPoints; ++i) {
-        points.emplace_back(interlacedArray, i);
     }
 
     // Send inputs to the GPU (Host to Device)
-    Scalar* interlacedArrayDevice;
     DataPoint* pointsDevice;
-    cudaMalloc( &interlacedArrayDevice, interlacedBufferSize );
-    cudaMalloc( &pointsDevice, pointBufferSize );
-    cudaMemcpy( interlacedArrayDevice, interlacedArray, interlacedBufferSize, cudaMemcpyHostToDevice);
-    cudaMemcpy( pointsDevice, points.data(), pointBufferSize, cudaMemcpyHostToDevice);
+    CUDA_CHECK(cudaMalloc( &pointsDevice, pointBufferSize ));
+    CUDA_CHECK(cudaMemcpy( pointsDevice, points.data(), pointBufferSize, cudaMemcpyHostToDevice));
 
     // Prepare output buffers
-    auto *potentialResults = new Scalar[nbPoints];
-    auto *gradientResults  = new Scalar[nbPoints*Dim];
+    auto* const potentialResults = new Scalar[nbPoints];
+    auto* const gradientResults  = new Scalar[nbPoints*Dim];
     Scalar* potentialResultsDevice;
     Scalar* gradientResultsDevice;
-    cudaMalloc(&potentialResultsDevice, scalarBufferSize);
-    cudaMalloc(&gradientResultsDevice , vectorBufferSize);
+    CUDA_CHECK(cudaMalloc(&potentialResultsDevice, scalarBufferSize));
+    CUDA_CHECK(cudaMalloc(&gradientResultsDevice , vectorBufferSize));
 
     // Set block and grid size depending on number of points
-    constexpr int blockSize = 128;
-    const int gridSize = (nbPoints + blockSize - 1) / blockSize;
-
-    // Binds the points to their internal values on the device
-    bindPointsKernel<DataPoint><<<gridSize, blockSize>>>(pointsDevice, interlacedArrayDevice, nbPoints);
-    cudaDeviceSynchronize();
+    constexpr unsigned int blockSize = 128;
+    const     unsigned int gridSize  = (nbPoints + blockSize - 1) / blockSize;
 
     // Compute the fitting in the kernel
     fitPotentialAndGradientKernel<DataPoint, MeanFitSmooth><<<gridSize, blockSize>>>(
         pointsDevice, nbPoints, analysisScale, potentialResultsDevice, gradientResultsDevice
     );
-    cudaDeviceSynchronize();
+    CUDA_CHECK(cudaDeviceSynchronize());
 
     // Fetch the results (Device to Host)
-    cudaMemcpy(potentialResults, potentialResultsDevice, scalarBufferSize, cudaMemcpyDeviceToHost);
-    cudaMemcpy(gradientResults , gradientResultsDevice , vectorBufferSize, cudaMemcpyDeviceToHost);
+    CUDA_CHECK(cudaMemcpy(potentialResults, potentialResultsDevice, scalarBufferSize, cudaMemcpyDeviceToHost));
+    CUDA_CHECK(cudaMemcpy(gradientResults , gradientResultsDevice , vectorBufferSize, cudaMemcpyDeviceToHost));
 
     // Free CUDA memory
-    cudaFree(interlacedArrayDevice);
-    cudaFree(potentialResultsDevice);
-    cudaFree(gradientResultsDevice);
+    CUDA_CHECK(cudaFree(pointsDevice));
+    CUDA_CHECK(cudaFree(potentialResultsDevice));
+    CUDA_CHECK(cudaFree(gradientResultsDevice));
 
     // Validate results
     const auto epsilon = Scalar(0.001);
-    for (int j = 0; j < nbPoints; ++j) {
-        const VectorType primGrad = extractVectorFromFlattenedArray<DataPoint>(j, gradientResults);
-        std::cout << "j:" << j << ", potential:"<< potentialResults[j] << ", ";
+    for (int i = 0; i < nbPoints; ++i) {
+        const VectorType primGrad = Eigen::Map< const VectorType >(gradientResults + Dim*i  );
+
+        std::cout << "i:" << i << ", potential:" << potentialResults[i] << ", ";
         std::cout << "primitiveGradient:" << primGrad.transpose() << " ; " << std::endl;
 
-        if (std::abs(potentialResults[j]) > epsilon || Scalar(1.) - std::abs(primGrad.dot(direction)) > epsilon) {
-            std::cerr << "Test  failed in " << __FILE__ << " (" << __LINE__ << ")" << std::endl;
+        if (std::abs(potentialResults[i]) > epsilon || Scalar(1.) - std::abs(primGrad.dot(direction)) > epsilon)
+        {
+            std::cerr << "Test failed in " << __FILE__ << " (" << __LINE__ << ")" << std::endl;
             abort();
         }
     }
 
-    delete[] interlacedArray;
     delete[] potentialResults;
     delete[] gradientResults;
 }
-
 
 __host__ int main(const int /*argc*/, char** /*argv*/) {
     std::cout << "Test plane fitting on CUDA..." << std::endl;
