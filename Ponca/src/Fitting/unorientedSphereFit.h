c46e2df6db10d827dd22e6970ffde4f90c5517b6
Nicolas Mellado <nmellado0@gmail.com>
2026-02-04 10:04:30 -0500

    [fitting] Add cast operator dNormalProvider

--- Ponca/src/Fitting/unorientedSphereFit.h
+++ Ponca/src/Fitting/unorientedSphereFit.h
@@ -110,6 +110,8 @@ public:
 
 public:
     PONCA_EXPLICIT_CAST_OPERATORS_DER(UnorientedSphereDerImpl,unorientedSphereDer)
+    PONCA_EXPLICIT_CAST_OPERATORS_DER(UnorientedSphereDerImpl,dNormalProvider) ///< get access to object providing dNormal
+
     PONCA_FITTING_DECLARE_INIT_ADDDER_FINALIZE
 
     PONCA_MULTIARCH inline ScalarArray dPotential() const;
