819eb28302365d2157c1ab8a0ca5b491adeed950
fauberva <auberval.florian01@gmail.com>
2026-02-19 17:41:57 +0100

    [examples] Removed unused binding kernel
    Signed-off-by: fauberva <auberval.florian01@gmail.com>

--- examples/cuda/cuda_utils.cu
+++ examples/cuda/cuda_utils.cu
@@ -7,29 +7,6 @@
     abort(); \
 }
 
-/*! \brief Kernel that binds each point buffer to the interlaced buffer of positions and normals in the Device.
- *
- * \tparam DataPoint The DataPoint type.
- * \tparam Scalar A scalar type (float, double...)
- * \param points The point cloud (needs to be bind to the interlacedArray on the device).
- * \param interlacedArray The interlaced buffer of positions and normals.
- * \param nbPoints The total number of points in the point cloud.
- */
-template<typename DataPoint, typename Scalar>
-__global__ void bindPointsKernel(
-    DataPoint* points,
-    Scalar* interlacedArray,
-    const int nbPoints
-) {
-    // Get global index
-    const unsigned int i = threadIdx.x + blockIdx.x * blockDim.x;
-
-    // Skip when not in the point cloud
-    if (i >= nbPoints) return;
-
-    points[i].bind(interlacedArray);
-}
-
 /*! \brief A KdTree Type that can be run on the GPU
  *
  * \warning The build function cannot be used in the CUDA kernel,
