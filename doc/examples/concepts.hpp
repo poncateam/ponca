241777dcf718f8635fd45e2ef9ccfbad72e85d61
Nicolas Mellado <nmellado0@gmail.com>
2026-03-17 09:48:55 -0400

    [doc] minor fixes and improvements

--- doc/examples/concepts.hpp
+++ doc/examples/concepts.hpp
@@ -17,8 +17,8 @@ namespace Ponca
             // \brief Defines the type used ton encode scalar values
             typedef float Scalar;
             // \brief Defines type used ton encode vector values
-            // 
-            // \note VectorType should have the same API than Eigen::Matrix API, 
+            //
+            // \note VectorType should have the same API than Eigen::Matrix API,
             // and be implicitly convertible to Eigen::Matrix
             typedef Eigen::Matrix<Scalar, Dim, 1> VectorType;
 
@@ -26,8 +26,8 @@ namespace Ponca
             PONCA_MULTIARCH inline PointConcept(const VectorType& pos = VectorType::Zero());
 
             // \brief Read access to the position property
-            // \note The return type should have the same API than VectorType (e.g. const Eigen::Map< const VectorType >&)
-            // and be implicitly convertible to Eigen::Matrix
+            // \note The return type should have the same API than VectorType (e.g. const Eigen::Map< const VectorType
+            // >&) and be implicitly convertible to Eigen::Matrix
             PONCA_MULTIARCH inline const VectorType& pos() const;
 
             // \brief Write access to the position property
@@ -35,7 +35,7 @@ namespace Ponca
             PONCA_MULTIARCH inline VectorType& pos();
         }; // class PointConcept
         //! [PointConcept]
-        
+
         //! [ComputationalObjectConcept]
         template <class DataPoint, class _NFilter, typename T = void>
         class ComputationalObjectConcept
@@ -48,6 +48,7 @@ namespace Ponca
                 Check = PROVIDES_CAPABILITY_1 && PROVIDES_CAPABILITY_2    //< List of required capabilities
                                                      PROVIDES_CAPABILITY, //< List of provided capabilities
             };
+
         public:
             using Scalar         = typename DataPoint::Scalar;     //< Inherited scalar type
             using VectorType     = typename DataPoint::VectorType; //< Inherited vector type
@@ -72,7 +73,7 @@ namespace Ponca
             // \param lQ Local coordinates of the point (e.g.: the difference between the point and the eval position)
             // \param pt The point as defined by the user
             // \return false if param nei is not a valid neighbour (weight = 0)
-            PONCA_MULTIARCH inline bool addLocalNeighbor(Scalar w, const VectorType &lQ, const DataPoint & pt);
+            PONCA_MULTIARCH inline bool addLocalNeighbor(Scalar w, const VectorType& lQ, const DataPoint& pt);
             // \return State of fitting
             // \warning Must be called be for any use of the fitting output
             PONCA_MULTIARCH inline FIT_RESULT finalize();
@@ -119,22 +120,86 @@ namespace Ponca
             /**************************************************************************/
             // Add a neighbor to perform the fit
             // \param w The weight of the point
-            // \param localQ Local coordinates of the point (e.g.: the difference between the point and the eval position)
+            // \param localQ Local coordinates of the point (e.g.: the difference between the point and the eval
+            // position)
             // \param attributes The point as defined by the user
             // \param dw The derivatives of the weight function w.r.t prescirbed parameter
             // \return false if param nei is not a valid neighbour (weight = 0)
-            PONCA_MULTIARCH inline bool addLocalNeighbor(Scalar w,
-                                                         const VectorType &localQ,
-                                                         const DataPoint &attributes,
-                                                         ScalarArray &dw);
+            PONCA_MULTIARCH inline bool addLocalNeighbor(Scalar w, const VectorType& localQ,
+                                                         const DataPoint& attributes, ScalarArray& dw);
             // Finalize the fitting procedure. This function is called after all neighbors have been processed.
             // \return State of fitting
             // \warning Must be called be for any use of the fitting output
             PONCA_MULTIARCH inline FIT_RESULT finalize();
         }; // class ComputationalDerivativesConcept
-        //! [ComputationalDerivativesConcept]
 
-        //! [WeightKernelConcept]
+        //! [ComputationalDerivativesConcept]
+        //! [FilterConcept]
+        // \brief Concept for neighborhood filters
+        //
+        // \see \ref CenteredNeighborhoodFrame
+        // \see \ref GlobalNeighborhoodFrame
+        template <class DataPoint>
+        class NeighborFilterConcept : public CenteredNeighborhoodFrame<DataPoint> // Or GlobalNeighborhoodFrame
+        {
+            /*! \brief Scalar type from DataPoint */
+            using Scalar = typename DataPoint::Scalar;
+            /*! \brief Vector type from DataPoint */
+            using VectorType = typename DataPoint::VectorType;
+            /*! \brief Matrix type from DataPoint */
+            using MatrixType = typename DataPoint::MatrixType;
+
+            // \brief Constructor
+            // A default constructor and a constructor from a vector and a scalar must be
+            // available. Even if parameter are unused or other are necessary.
+            // \param v The evaluation location
+            // \param t The radius
+            NeighborFilterConcept(const VectorType& v = VectorType::Zero(), Scalar_ t = 0);
+            /*!
+                \brief Compute the weight of the given query, which is always $1$
+                \param _q Query in global coordinate system
+            */
+            PONCA_MULTIARCH inline WeightReturnType operator()(const DataPoint& _q) const;
+
+            /*!
+                \brief First order derivative in space (for each spatial dimension \f$\mathsf{x})\f$, which are always
+               $0$
+                \param _q Query location in global coordinate system
+                \param _attributes Query in global coordinate system
+            */
+            PONCA_MULTIARCH inline VectorType spacedw(const VectorType& _q, const DataPoint& _attributes);
+
+            /*!
+                \brief Second order derivative in space (for each spatial dimension \f$\mathsf{x})\f$, which are always
+               $0$
+                \param _q Query in global coordinate
+                \param _attributes Query in global coordinate system
+            */
+            PONCA_MULTIARCH inline MatrixType spaced2w(const VectorType& _q, const DataPoint& _attributes) const;
+
+            /*!
+                \brief First order derivative in scale  \f$t\f$, which are always $0$
+                \param _q Query in global coordinate
+                \param _attributes Query in global coordinate system
+            */
+            PONCA_MULTIARCH inline Scalar scaledw(const VectorType& _q, const DataPoint& _attributes) const;
+
+            /*!
+                \brief Second order derivative in scale  \f$t\f$, which are always $0$
+                \param _q Query in global coordinate
+                \param _attributes Query in global coordinate system
+            */
+            PONCA_MULTIARCH inline Scalar scaled2w(const VectorType& _q, const DataPoint& _attributes) const;
+
+            /*!
+                \brief Cross derivative in scale \f$t\f$ and in space (for each spatial dimension \f$\mathsf{x})\f$,
+               which are always $0$
+                \param _q Query in global coordinate
+                \param _attributes Query in global coordinate system
+            */
+            PONCA_MULTIARCH inline VectorType scaleSpaced2w(const VectorType& _q, const DataPoint& _attributes) const;
+        };
+
         // \brief Concept of a 1D weighting function and its derivatives.
         template <typename _Scalar>
         class WeightKernelConcept
