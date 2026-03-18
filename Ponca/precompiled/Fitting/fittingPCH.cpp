c4a21d4295878119ce2b16d40ff7c57b139581ed
fauberva <florian.auberval@irit.fr>
2026-03-18 14:01:02 +0100

    [cmake] Test PCH + ETI
    Signed-off-by: fauberva <florian.auberval@irit.fr>

--- Ponca/precompiled/Fitting/fittingPCH.cpp
+++
@@ -1,6 +0,0 @@
-#include "fittingPCH.h"
-
-/*!
-  \file fittingPCH.cpp
-  \brief Precompiled header target that defines the commonly used Fitting types for 3D point clouds.
-*/
--- Ponca/precompiled/Fitting/fittingPCH.h
+++ Ponca/precompiled/Fitting/fittingPCH.h
@@ -1,19 +1,9 @@
 #pragma once
 
-#include "fittingETI.h" // For Explicit Template Instantiations
-
-#define EXTERN
-#include "fittingDeclareMacro.h"
+#include "fittingETI.h" // References Explicit Template Instantiations
+// TODO : Include other heavy to compile headers
 
 /*!
   \file fittingPCH.cpp
   \brief Precompiled header that defines the commonly used Fitting types for 3D point clouds.
 */
-
-// Explicit template instantiation
-FITTING_DEF(float)
-FITTING_DEF(double)
-FITTING_DEF(long double)
-
-#undef FITTING_DEF
-#undef EXTERN
