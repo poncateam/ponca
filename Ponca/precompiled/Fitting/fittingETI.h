0a14f2a74eeb674f258c51ec79b249a42df8fcbe
fauberva <florian.auberval@irit.fr>
2026-03-24 09:22:30 +0100

    [precompiled] Combine PCH and ETI lib and rework comments

--- Ponca/precompiled/Fitting/fittingETI.h
+++ Ponca/precompiled/Fitting/fittingETI.h
@@ -1,14 +1,19 @@
+/*
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+/*!
+ * \file Ponca/precompiled/Fitting/fittingETI.h
+ * \brief Instantiate explicitly most fitting types.
+ * It tells the compiler that it should fetch the Fitting types from the precompiled library.
+ */
+
 #pragma once
 
 #define EXTERN extern
 #include "fittingDeclareMacro.h"
-
-/*!
-  \file fittingETI.h
-  \brief Predeclare the Explicit Template Instantiations that exist in the precompiled library.
-  This header is to be included in each target that wants to benefit from precompiled Fitting acceleration.
-  It tells the compiler that it should fetch the Fitting types from the precompiled library.
-*/
 
 FITTING_DEF(float)
 FITTING_DEF(double)
--- Ponca/precompiled/Fitting/fittingPCH.h
+++ Ponca/precompiled/Fitting/fittingPCH.h
@@ -1,9 +1,16 @@
+/*
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+/*!
+ * \file Ponca/precompiled/Fitting/fittingPCH.h
+ * \brief Precompilable header that defines the commonly used Fitting types for 3D point clouds.
+ * This header is to be included in each target that wants to benefit from precompiled Fitting acceleration.
+ */
+
 #pragma once
 
 #include "fittingETI.h" // References Explicit Template Instantiations
 // TODO : Include other heavy to compile headers
-
-/*!
-  \file fittingPCH.cpp
-  \brief Precompiled header that defines the commonly used Fitting types for 3D point clouds.
-*/
