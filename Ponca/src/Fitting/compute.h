c5086a0ab343d1a61da6b9c1ea0239ac397c1e7d
Bastien DOIGNIES <bastien.doignies@liris.cnrs.fr>
2026-03-31 11:06:25 +0200

    [Fitting] Add new ComputeScheme class

--- Ponca/src/Fitting/compute.h
+++ Ponca/src/Fitting/compute.h
@@ -63,6 +63,59 @@ namespace Ponca
             return UNDEFINED;
         };
     }; // struct ComputeObject
+    
+    /**
+     * \brief ComputeScheme serves as an interface to reuse and modify the result of ComputeObject::compute
+     *
+     * This class is not templated on the ComputeObject to allow easier comparison with same scheme and 
+     * parameters but different objects.
+     */
+    struct ComputeScheme
+    {
+    public:
+#ifdef PONCA_CPU_ARCH
+        /*! \brief Convenience function for STL-like container
+         *
+         * \note This method is only accessible when using a CPU architecture (PONCA_CPU_ARCH = true)
+         * \tparam ComputeObject Compute object to call the function on
+         * \tparam Container And STL-Like container
+         * \see #compute(const IteratorBegin& begin, const IteratorEnd& end)
+         */
+        template <typename ComputeObject, typename Container>
+        FIT_RESULT compute(ComputeObject& co, const Container& c)
+        {
+            return co.compute(std::begin(c), std::end(c));
+        }
+#endif
+
+        /*! \brief Convenience function for STL-like iterators
+            \tparam ComputeObject Compute object to call the function on
+            \tparam IteratorBegin The beginning of the iterator (std::begin(iterator)
+            \tparam IteratorEnd   The end of the iterator (std::end(iterator)
+        */
+        template <typename ComputeObject, typename IteratorBegin, typename IteratorEnd>
+        PONCA_MULTIARCH inline FIT_RESULT compute(ComputeObject& co, const IteratorBegin& begin, const IteratorEnd& end)
+        {
+            return co.compute(begin, end);
+        };
+
+        /*! \brief Convenience function to iterate over a subset of samples in a PointContainer
+            \tparam ComputeObject Compute object to call the function on
+            \tparam IndexRange STL-Like range storing indices of the neighbors
+            \tparam PointContainer STL-like container storing the points
+            \see #compute(const IteratorBegin& begin, const IteratorEnd& end)
+        */
+        template <typename ComputeObject, typename IndexRange, typename PointContainer>
+        PONCA_MULTIARCH inline FIT_RESULT computeWithIds(ComputeObject& co, IndexRange ids, const PointContainer& points)
+        {
+            return co.computeWithIds(ids, points);
+        };
+
+    private:
+    };
+    
+    // This class allows for a unified interface with other methods.
+    using SimpleCompute = ComputeScheme;
 
 } // namespace Ponca
 
