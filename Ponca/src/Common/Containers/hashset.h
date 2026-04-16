7552e23edbeaf0dcaffb4f8768fc48c7676e1cc8
fauberva <auberval.florian01@gmail.com>
2026-04-16 16:09:27 +0200

    [common] Comment changes
    Signed-off-by: fauberva <auberval.florian01@gmail.com>

--- Ponca/src/Common/Containers/hashset.h
+++ Ponca/src/Common/Containers/hashset.h
@@ -1,5 +1,5 @@
 /**
-This Source Code Form is subject to the terms of the Mozilla Public
+ This Source Code Form is subject to the terms of the Mozilla Public
  License, v. 2.0. If a copy of the MPL was not distributed with this
  file, You can obtain one at http://mozilla.org/MPL/2.0/.
  \author Auberval Florian
@@ -10,18 +10,35 @@ This Source Code Form is subject to the terms of the Mozilla Public
 #include "../defines.h"
 
 namespace Ponca {
-    /*! \brief An HashMap implementation, that stores the internal values in an array of signed values.
+    /*! \brief A simple HashMap implementation, that stores unique elements in an array of signed values.
      *
-     * The internal values stored must not be equal to -1
-     * For the search, it's best case complexity is O(1), and the worst case complexity is O(n).
+     * The internal values stored must not be equal to -1, because it is used as an empty flag by the HashSet.
+     * For the search, the best case complexity is O(1), and the worst case complexity is O(n).
+     * The complexity is entirely dependent on the hashing function, and the given values :
+     * Sparser hashing results will reduce the searching complexity.
+     *
      * \tparam N The maximum size of the HashSet
-     * \tparam T The value type stored in the HashSet : Must be a signed value type that can be used as an array index (int-like)
+     * \tparam T The value type stored in the HashSet : Must be a signed value type that can be used as an array index
+     * (int-like)
      */
     template <int N, typename T>
     class HashSet {
     public:
+        /*! \brief Empty the array
+         *
+         * Iterates over every element and sets its value to -1
+         */
         PONCA_MULTIARCH void clear();
-        PONCA_MULTIARCH bool insert(int key);
+
+        /*! \brief Tries to insert a value in the HashSet
+         *
+         * When the hashing function provokes a collision (if two values have the same storage address in the data
+         * array), the HashSet will store the value in the next available array address.
+         * \param value The value to be inserted in the HashSet
+         * \return True if the value was inserted successfully, and false if the value was already inserted or if the
+         * HashSet is full
+         */
+        PONCA_MULTIARCH bool insert(int value);
 
     private:
         static constexpr T EMPTY = T(-1);
@@ -41,23 +58,28 @@ namespace Ponca {
     }
 
     template <int N, typename T>
-    PONCA_MULTIARCH bool HashSet<N, T>::insert(const int key)
+    PONCA_MULTIARCH bool HashSet<N, T>::insert(const int value)
     {
-        const int h = hash(key);
+        const int h = hash(value);
 
+        // Try to insert
         for (int i = 0; i < N; ++i) {
             int idx = (h + i) % N;
 
             int& slot = table[idx];
 
+            // Stores here if the address is empty
             if (slot == EMPTY) {
-                slot = key;
+                slot = value;
                 return true;
             }
-            if (slot == key) {
+            // The value was already inserted in the array
+            if (slot == value) {
                 return false;
             }
         }
-        return false; // full
+
+        // The array is full
+        return false;
     }
 }
