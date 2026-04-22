405714c0b5d1df333f92aed6108a68c5a00f7ffa
fauberva <florian.auberval@irit.fr>
2026-04-22 15:48:45 +0200

    [common] Add more comments to doc of `HashSet` and `BitSet`
    - Uncomment PONCA_ASSERT_MSG in BitSet
    
    Signed-off-by: fauberva <florian.auberval@irit.fr>

--- Ponca/src/Common/Containers/bitset.h
+++ Ponca/src/Common/Containers/bitset.h
@@ -12,18 +12,24 @@
 
 namespace Ponca
 {
-    /*! \brief BitSet implementation, similar to std::bitset
+    /*! \brief A simple BitSet implementation that mimics a set of indices.
+     * The internal logic of this bitset is similar to `std::bitset`, but is compatible with CUDA.
      *
      * Allows to insert and search in O(1) complexity, but is memory expensive, because we allocate a single
      * bit for each possible indices, to flag if it was inserted or not, which is not ideal for large amount of indices.
      *
-     * The memory use should be :
+     * The memory use of the BitSet depending on N should be :
      * Bitset<10000> → 10k bits = 1.25 KB
      * Bitset<1e6>   → 125 KB
      * Bitset<1e7>   → 1.25 MB
      *
      * \tparam N Maximum number of indices
-     * \tparam T The data type of the array storing the bits. Default to unsigned long long for 64 bits storage.
+     * \tparam T The data type of the array storing the bits. Default to 'unsigned long long' for 64 bits storage.
+     *
+     * \see `BitSet::erase`, `BitSet::insert` for set-index-like methods
+     *
+     * \warning The inserted values must always be smaller than N, because we are using them as indices to store the
+     * bits inside the BitSet.
      */
     template <int N, typename T = unsigned long long>
     class BitSet
@@ -32,9 +38,37 @@ namespace Ponca
 
     public:
         PONCA_MULTIARCH BitSet();
-        PONCA_MULTIARCH void flip(int index);
-        PONCA_MULTIARCH bool insert(int index);
-        PONCA_MULTIARCH [[nodiscard]] bool find(int index) const;
+
+        /*! \brief Toggles the value of a bit
+         * \param i The bit to flip
+         */
+        PONCA_MULTIARCH void flip(int i);
+
+        /*! \brief Tries to insert a value in the set
+         *
+         * \param value The value to be removed in the set. Must always be smaller than N, because we are using the
+         * values as indices inside the BitSet.
+         * \return True if the value was removed successfully, and false if the value was already not inside the Set
+         */
+        PONCA_MULTIARCH bool erase(int value);
+
+        /*! \brief Tries to insert a value in the set
+         *
+         * \param value The value to be inserted in the HashSet. Must always be smaller than N, because we are using the
+         * values as indices inside the BitSet.
+         * \return True if the value was inserted successfully, and false if the value was already inserted or if the
+         * HashSet is full
+         */
+        PONCA_MULTIARCH bool insert(int value);
+
+        /*! \brief Search if the value was already inserted or not
+         *
+         * \param value The value to search for (search is O(N) because it corresponds to the index in the BitSet)
+         * \return
+         */
+        PONCA_MULTIARCH [[nodiscard]] bool find(int value) const;
+
+        //! \brief Sets all the bits to EMPTY
         PONCA_MULTIARCH void clear();
 
     protected:
@@ -60,11 +94,24 @@ namespace Ponca
     }
 
     template <int N, typename T>
-    PONCA_MULTIARCH bool BitSet<N, T>::insert(const int index)
+    PONCA_MULTIARCH bool BitSet<N, T>::erase(const int value)
     {
-        // PONCA_DEBUG_ASSERT(index >= 0 && index < N);
-        const int byte             = index / BIT_SIZE;
-        const int bit              = index % BIT_SIZE;
+        PONCA_ASSERT_MSG(value >= 0 && value < N,
+                         "Attempted to remove a value that is outside the scope of the BitSet");
+        const int byte          = value / BIT_SIZE;
+        const int bit           = value % BIT_SIZE;
+        const T bitMask         = (T(1) << bit);
+        const bool alreadyEmpty = (m_data[byte] & bitMask) != 0;
+        m_data[byte] &= ~bitMask;
+        return alreadyEmpty;
+    }
+
+    template <int N, typename T>
+    PONCA_MULTIARCH bool BitSet<N, T>::insert(const int value)
+    {
+        PONCA_ASSERT_MSG(value >= 0 && value < N, "Inserted value is outside the scope of the BitSet");
+        const int byte             = value / BIT_SIZE;
+        const int bit              = value % BIT_SIZE;
         const T bitMask            = (T(1) << bit);
         const bool alreadyInserted = (m_data[byte] & bitMask) == 0;
         m_data[byte] |= bitMask;
@@ -72,20 +119,20 @@ namespace Ponca
     }
 
     template <int N, typename T>
-    PONCA_MULTIARCH void BitSet<N, T>::flip(const int index)
+    PONCA_MULTIARCH void BitSet<N, T>::flip(const int i)
     {
-        // PONCA_DEBUG_ASSERT(index >= 0 && index < N);
-        const int byte = index / BIT_SIZE;
-        const int bit  = index % BIT_SIZE;
+        PONCA_ASSERT_MSG(i >= 0 && i < N, "Flipped value is outside the scope of the BitSet");
+        const int byte = i / BIT_SIZE;
+        const int bit  = i % BIT_SIZE;
         m_data[byte] ^= (T(1) << bit);
     }
 
     template <int N, typename T>
-    PONCA_MULTIARCH [[nodiscard]] bool BitSet<N, T>::find(const int index) const
+    PONCA_MULTIARCH [[nodiscard]] bool BitSet<N, T>::find(const int value) const
     {
-        // PONCA_DEBUG_ASSERT(index >= 0 && index < N);
-        const int byte = index / BIT_SIZE;
-        const int bit  = index % BIT_SIZE;
+        PONCA_ASSERT_MSG(value >= 0 && value < N, "Searched value is outside the scope of the BitSet");
+        const int byte = value / BIT_SIZE;
+        const int bit  = value % BIT_SIZE;
         return (m_data[byte] & (T(1) << bit)) != 0;
     }
 } // namespace Ponca
--- Ponca/src/Common/Containers/hashset.h
+++ Ponca/src/Common/Containers/hashset.h
@@ -11,15 +11,23 @@
 
 namespace Ponca
 {
-    /*! \brief A simple HashMap implementation, that stores unique elements in an array of signed values.
+    /*! \brief A simple HashMap implementation that mimics a set of indices, by only storing the keys (Hence the name
+     * HashSet). The internal logic of this HashMap is similar to a `std::unordered_map`, but is compatible with CUDA.
      *
-     * The internal values stored must not be equal to -1, because it is used as an empty flag by the HashSet.
+     * Stores unique signed integer values in a contiguous array.
+     *
+     * \warning The internal values stored must not be equal to -1, because it is used as an empty flag by the HashSet.
+     *
      * For the search, the best case complexity is O(1), and the worst case complexity is O(n).
      * The complexity is entirely dependent on the hashing function, and the given values :
-     * Sparser hashing results will reduce the searching complexity.
+     * If our hashing function produce sparser result for our set of indices, it will reduce the complexity of the
+     * searches, and therefore improve the performance of our algorithm.
+     *
+     * \see HashSet::hash for the hashing function
      *
      * \tparam N The maximum size of the HashSet
-     * \tparam T The value type stored in the HashSet : Must be a signed integer-like type
+     * \tparam T The value type stored in the HashSet : Must be a signed integer type, because the values are used as
+     * indices for the array
      */
     template <int N, typename T = int>
     class HashSet
@@ -35,19 +43,35 @@ namespace Ponca
 
         /*! \brief Tries to insert a value in the HashSet
          *
-         * When the hashing function provokes a collision (if two values have the same storage address in the data
-         * array), the HashSet will store the value in the next available array address.
+         * \note The hashing function doesn't guarantee that we always get different results, sometimes two different
+         * values can happen to have the same storage address in our data array. When the hashing function provokes a
+         * collision with another value, the HashSet will store the other value in the next available array address. The
+         * search will have to keep looking for the value if it isn't stored at the hashing result, which is why the
+         * search isn't always of a O(1) complexity.
+         *
          * \param value The value to be inserted in the HashSet
          * \return True if the value was inserted successfully, and false if the value was already inserted or if the
          * HashSet is full
          */
         PONCA_MULTIARCH bool insert(int value);
 
+        /*! \brief Tries to find a value in the HashSet
+         *
+         * \param value The value to search for
+         * \return True if the value is inside the HashSet, false if it's not in the HashSet.
+         */
+        PONCA_MULTIARCH bool find(int value);
+
     private:
         static constexpr T EMPTY = T(-1);
         T table[N]               = {};
 
-        PONCA_MULTIARCH [[nodiscard]] static int hash(const int x) { return (x * 2654435761u) % N; }
+        //! \brief The hashing function : (x * 2654435761u) % N
+        PONCA_MULTIARCH [[nodiscard]] static int hash(const int x)
+        {
+            PONCA_ASSERT_MSG(x >= 0 && x < N, "Index is outside the scope of the HashSet");
+            return (x * 2654435761u) % N;
+        }
     };
 
     template <int N, typename T>
@@ -82,6 +106,33 @@ namespace Ponca
         }
 
         // The array is full
+        return false;
+    }
+
+    template <int N, typename T>
+    PONCA_MULTIARCH bool HashSet<N, T>::find(const int value)
+    {
+        const int h = hash(value);
+
+        // Try to find the value
+        for (int i = 0; i < N; ++i)
+        {
+            const int idx = (h + i) % N;
+            T& slot       = table[idx]; // Get the address
+
+            // Stops the search here if the address is empty
+            if (slot == EMPTY)
+            {
+                return false;
+            }
+            // Value found
+            if (slot == value)
+            {
+                return true;
+            }
+            // The value might have been inserted elsewhere, keep looking...
+        }
+        // Value not in HashSet
         return false;
     }
 } // namespace Ponca
