d8687e36b6d6c81f4657bf3511dc94a8fd3d32f1
Bastien DOIGNIES <bastien.doignies@liris.cnrs.fr>
2026-06-15 17:41:41 +0200

    [fitting] First working factory (+test)

---
+++ tests/src/factory.cpp
@@ -0,0 +1,26 @@
+#include <Ponca/Ponca>
+#include <Ponca/src/Fitting/Factory/factory.h>
+
+#include "../common/testing.h"
+#include "../common/testUtils.h"
+
+int main(int argc, char** argv)
+{
+    if (!init_testing(argc, argv))
+    {
+        return EXIT_FAILURE;
+    }
+
+    using namespace Ponca;
+    using Point = PointPosition<double, 2>;
+    using NF    = DistWeightFilter<Point, SmoothWeightKernel<double>>;
+    using F     = Factory<Point, NF, 0>;
+
+    F::foreach ([](const auto& x) { std::cout << x.name << std::endl; });
+
+    auto spheres = F::Filter<AlgebraicSphereProvider>();
+    spheres.foreach ([](const auto& x) { std::cout << x.object.isPlane() << std::endl; });
+
+    auto m = spheres.GetMethod<Method::APSS>();
+    std::cout << m.name << std::endl;
+}
